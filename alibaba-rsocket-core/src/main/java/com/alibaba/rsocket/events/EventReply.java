7ae56e534d053417fd395b625b14f69eefc06914
leijuan <jacky.chenlb@alibaba-inc.com>
2020-01-09 13:36:59 -0800

    Event Reply

---
+++ alibaba-rsocket-core/src/main/java/com/alibaba/rsocket/events/EventReply.java
@@ -0,0 +1,54 @@
+package com.alibaba.rsocket.events;
+
+/**
+ * Event Reply
+ *
+ * @author leijuan
+ */
+public class EventReply {
+    private String eventId;
+    private String replyId;
+    private String replyName;
+    private Integer status;
+    private String data;
+
+    public String getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(String eventId) {
+        this.eventId = eventId;
+    }
+
+    public String getReplyId() {
+        return replyId;
+    }
+
+    public void setReplyId(String replyId) {
+        this.replyId = replyId;
+    }
+
+    public String getReplyName() {
+        return replyName;
+    }
+
+    public void setReplyName(String replyName) {
+        this.replyName = replyName;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
