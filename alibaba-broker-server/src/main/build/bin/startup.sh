683bf03982aafc062fa538ee42a9e45e8431e788
leijuan <jacky.chenlb@alibaba-inc.com>
2020-01-27 14:09:16 -0800

    remove logging config

--- alibaba-broker-server/src/main/build/bin/startup.sh
+++ alibaba-broker-server/src/main/build/bin/startup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 PRGDIR=$(dirname "$0")
 jar_file=alibaba-rsocket-broker.jar
-JAVA_OPTS="${JAVA_OPTS} -Dlogback.configurationFile=${PRGDIR}/config/logback-spring.xml"
 mkdir -p logs
 nohup java ${JAVA_OPTS} -jar "${PRGDIR}"/lib/${jar_file} >> "${PRGDIR}"/logs/application.log 2>&1 &
 echo $! > "${PRGDIR}"/app.pid
