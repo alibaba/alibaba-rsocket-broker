efe4eb8d14c5ce8ef1d2cf4feeed55361784656c
leijuan <jacky.chenlb@alibaba-inc.com>
2020-01-30 14:22:03 -0800

    extends RuntimeException

--- alibaba-rsocket-core/src/main/java/com/alibaba/rsocket/encoding/EncodingException.java
+++ alibaba-rsocket-core/src/main/java/com/alibaba/rsocket/encoding/EncodingException.java
@@ -1,23 +1,14 @@
 package com.alibaba.rsocket.encoding;
 
-import io.rsocket.exceptions.RSocketException;
-
 /**
  * Encoding Exception
  *
  * @author linux_china
  */
-public class EncodingException extends RSocketException {
-    public EncodingException(String message) {
-        super(message);
-    }
+public class EncodingException extends RuntimeException {
 
     public EncodingException(String message, Throwable cause) {
         super(message, cause);
     }
 
-    @Override
-    public int errorCode() {
-        return 0x00000501;
-    }
 }
