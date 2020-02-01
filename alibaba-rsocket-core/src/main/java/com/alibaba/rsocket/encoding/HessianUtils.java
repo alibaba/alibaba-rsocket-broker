a710771d138b367c7c0faadbbf9085b34c79e323
leijuan <jacky.chenlb@alibaba-inc.com>
2020-02-01 14:22:14 -0800

    remove unnecessary methods

--- alibaba-rsocket-core/src/main/java/com/alibaba/rsocket/encoding/HessianUtils.java
+++ alibaba-rsocket-core/src/main/java/com/alibaba/rsocket/encoding/HessianUtils.java
@@ -6,7 +6,6 @@ import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufInputStream;
 import io.netty.buffer.Unpooled;
 
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 
 /**
@@ -16,47 +15,15 @@ import java.io.ByteArrayOutputStream;
  */
 public class HessianUtils {
 
-    public static byte[] output(Object obj) {
-        if (obj == null) {
-            return new byte[]{};
-        }
-        try {
-            ByteArrayOutputStream bos = new ByteArrayOutputStream();
-            HessianSerializerOutput output = new HessianSerializerOutput(bos);
-            output.writeObject(obj);
-            output.flush();
-            return bos.toByteArray();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public static ByteBuf outputAsBuffer(Object obj) {
+    public static ByteBuf outputAsBuffer(Object obj) throws Exception {
         if (obj == null) {
             return Unpooled.EMPTY_BUFFER;
         }
-        try {
-            ByteArrayOutputStream bos = new ByteArrayOutputStream();
-            HessianSerializerOutput output = new HessianSerializerOutput(bos);
-            output.writeObject(obj);
-            output.flush();
-            return Unpooled.wrappedBuffer(bos.toByteArray());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public static Object decode(byte[] content) {
-        try {
-            ByteArrayInputStream bis = new ByteArrayInputStream(content);
-            HessianSerializerInput input = new HessianSerializerInput(bis);
-            return input.readObject();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        HessianSerializerOutput output = new HessianSerializerOutput(bos);
+        output.writeObject(obj);
+        output.flush();
+        return Unpooled.wrappedBuffer(bos.toByteArray());
     }
 
     public static Object decode(ByteBuf buffer) throws Exception {
