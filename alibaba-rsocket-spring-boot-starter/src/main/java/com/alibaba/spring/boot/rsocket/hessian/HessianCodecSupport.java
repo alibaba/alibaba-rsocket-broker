b404a951d91c8d096f7cfb978d159232b2beb88b
leijuan <jacky.chenlb@alibaba-inc.com>
2020-02-01 14:53:56 -0800

    Hessian Codec support

---
+++ alibaba-rsocket-spring-boot-starter/src/main/java/com/alibaba/spring/boot/rsocket/hessian/HessianCodecSupport.java
@@ -0,0 +1,34 @@
+package com.alibaba.spring.boot.rsocket.hessian;
+
+import com.caucho.hessian.io.HessianSerializerInput;
+import com.caucho.hessian.io.HessianSerializerOutput;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferFactory;
+import org.springframework.util.MimeType;
+import org.springframework.util.MimeTypeUtils;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Hessian Codec support
+ *
+ * @author leijuan
+ */
+public class HessianCodecSupport {
+    public static MimeType HESSIAN_MIME_TYPE = MimeTypeUtils.parseMimeType("application/x-hessian");
+    public static List<MimeType> HESSIAN_MIME_TYPES = Collections.singletonList(HESSIAN_MIME_TYPE);
+
+
+    public Object decode(DataBuffer dataBuffer) throws Exception {
+        return new HessianSerializerInput(dataBuffer.asInputStream()).readObject();
+    }
+
+    public DataBuffer encode(Object obj, DataBufferFactory bufferFactory) throws Exception {
+        DataBuffer dataBuffer = bufferFactory.allocateBuffer();
+        HessianSerializerOutput output = new HessianSerializerOutput(dataBuffer.asOutputStream());
+        output.writeObject(obj);
+        output.flush();
+        return dataBuffer;
+    }
+}
