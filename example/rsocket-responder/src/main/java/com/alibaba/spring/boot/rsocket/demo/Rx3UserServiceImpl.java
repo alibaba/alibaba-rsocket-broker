527d492a629e339d810ca727b982da6cb9022279
leijuan <jacky.chenlb@alibaba-inc.com>
2020-01-31 17:23:13 -0800

    Rx3UserService implementation

---
+++ example/rsocket-responder/src/main/java/com/alibaba/spring/boot/rsocket/demo/Rx3UserServiceImpl.java
@@ -0,0 +1,36 @@
+package com.alibaba.spring.boot.rsocket.demo;
+
+import com.alibaba.spring.boot.rsocket.RSocketService;
+import com.alibaba.user.Rx3UserService;
+import com.alibaba.user.User;
+import com.github.javafaker.Faker;
+import io.reactivex.rxjava3.core.Flowable;
+import io.reactivex.rxjava3.core.Maybe;
+
+/**
+ * Rx3UserService implementation
+ *
+ * @author linux_china
+ */
+@RSocketService(serviceInterface = Rx3UserService.class)
+public class Rx3UserServiceImpl implements Rx3UserService {
+    private Faker faker = new Faker();
+
+    @Override
+    public Maybe<User> findById(Integer id) {
+        return Maybe.fromCallable(() -> {
+            User user = new User();
+            user.setId(id);
+            user.setNick(faker.name().name());
+            user.setPhone(faker.phoneNumber().cellPhone());
+            user.setEmail(faker.internet().emailAddress());
+            return user;
+        });
+    }
+
+    @Override
+    public Flowable<User> findAllPeople(String type) {
+        return Flowable.range(0, 10)
+                .map(id -> new User(id, "nick:" + type));
+    }
+}
