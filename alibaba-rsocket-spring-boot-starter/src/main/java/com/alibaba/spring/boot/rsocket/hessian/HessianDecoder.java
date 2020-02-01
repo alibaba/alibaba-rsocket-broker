3fe38caa6c7a62aff9fc8115d3c005b2112c3fc8
leijuan <jacky.chenlb@alibaba-inc.com>
2020-02-01 14:54:07 -0800

    Hessian Decoder

---
+++ alibaba-rsocket-spring-boot-starter/src/main/java/com/alibaba/spring/boot/rsocket/hessian/HessianDecoder.java
@@ -0,0 +1,53 @@
+package com.alibaba.spring.boot.rsocket.hessian;
+
+import org.reactivestreams.Publisher;
+import org.springframework.core.ResolvableType;
+import org.springframework.core.codec.Decoder;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.util.MimeType;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Hessian Decoder
+ *
+ * @author leijuan
+ */
+@SuppressWarnings("NullableProblems")
+public class HessianDecoder extends HessianCodecSupport implements Decoder<Object> {
+
+    @Override
+    public boolean canDecode(ResolvableType elementType, MimeType mimeType) {
+        return HESSIAN_MIME_TYPE.equals(mimeType);
+    }
+
+    @Override
+    public Flux<Object> decode(Publisher<DataBuffer> inputStream, ResolvableType elementType, MimeType mimeType, Map<String, Object> hints) {
+        return Flux.from(inputStream).handle(((dataBuffer, sink) -> {
+            try {
+                sink.next(decode(dataBuffer));
+            } catch (Exception e) {
+                sink.error(e);
+            }
+        }));
+    }
+
+    @Override
+    public Mono<Object> decodeToMono(Publisher<DataBuffer> inputStream, ResolvableType elementType, MimeType mimeType, Map<String, Object> hints) {
+        return Mono.from(inputStream).handle((dataBuffer, sink) -> {
+            try {
+                sink.next(decode(dataBuffer));
+            } catch (Exception e) {
+                sink.error(e);
+            }
+        });
+    }
+
+    @Override
+    public List<MimeType> getDecodableMimeTypes() {
+        return HESSIAN_MIME_TYPES;
+    }
+}
