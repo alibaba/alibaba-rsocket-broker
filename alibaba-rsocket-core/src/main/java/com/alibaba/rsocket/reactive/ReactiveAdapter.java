edd2b72e783b49f250242a1c0bc02949d4bce17c
leijuan <jacky.chenlb@alibaba-inc.com>
2020-01-31 17:00:07 -0800

    Reactive Adapter to Reactor

---
+++ alibaba-rsocket-core/src/main/java/com/alibaba/rsocket/reactive/ReactiveAdapter.java
@@ -0,0 +1,34 @@
+package com.alibaba.rsocket.reactive;
+
+import com.alibaba.rsocket.MutableContext;
+import org.jetbrains.annotations.Nullable;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+/**
+ * Reactive Adapter to Reactor
+ *
+ * @author leijuan
+ */
+public interface ReactiveAdapter {
+
+    <T> Mono<T> toMono(@Nullable Object source);
+
+    <T> Flux<T> toFlux(@Nullable Object source);
+
+    Object fromPublisher(Mono<?> mono, Class<?> returnType, MutableContext mutableContext);
+
+    Object fromPublisher(Flux<?> flux, Class<?> returnType, MutableContext mutableContext);
+
+    static ReactiveAdapter findAdapter(String returnTypeName) {
+        if (returnTypeName.equals("java.util.concurrent.CompletableFuture")) {
+            return ReactiveAdapterFuture.getInstance();
+        } else if (returnTypeName.startsWith("io.reactivex.rxjava3.")) {
+            return ReactiveAdapterRxJava3.getInstance();
+        } else if (returnTypeName.startsWith("io.reactivex.")) {
+            return ReactiveAdapterRxJava2.getInstance();
+        } else {
+            return ReactiveAdapterDefault.getInstance();
+        }
+    }
+}
