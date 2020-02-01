a4feeea1e744fea8aba9b3011d996bcf06b2586b
leijuan <jacky.chenlb@alibaba-inc.com>
2020-01-31 17:36:17 -0800

    use rxjava3 api

--- example/rsocket-requester/src/main/java/com/alibaba/spring/boot/rsocket/demo/Rx3PortalController.java
+++ example/rsocket-requester/src/main/java/com/alibaba/spring/boot/rsocket/demo/Rx3PortalController.java
@@ -1,14 +1,13 @@
 package com.alibaba.spring.boot.rsocket.demo;
 
-import com.alibaba.rsocket.reactive.rxjava3.RxJava3Adapter;
 import com.alibaba.user.Rx3UserService;
 import com.alibaba.user.User;
+import io.reactivex.rxjava3.core.Maybe;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
 
 /**
  * rx3 portal demo for test
@@ -22,8 +21,8 @@ public class Rx3PortalController {
     Rx3UserService rx3UserService;
 
     @GetMapping("/user/{id}")
-    public Mono<User> user(@PathVariable Integer id) {
-        return RxJava3Adapter.maybeToMono(rx3UserService.findById(id));
+    public Maybe<User> user(@PathVariable Integer id) {
+        return rx3UserService.findById(id);
     }
 
     @RequestMapping("/")
