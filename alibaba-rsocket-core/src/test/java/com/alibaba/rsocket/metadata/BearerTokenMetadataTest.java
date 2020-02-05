67fdde2ed4c77c0f3b14eaecf2587b62e9b11b5e
leijuan <jacky.chenlb@alibaba-inc.com>
2020-02-05 14:02:21 -0800

    add validation with AuthMetadataFlyweight

--- alibaba-rsocket-core/src/test/java/com/alibaba/rsocket/metadata/BearerTokenMetadataTest.java
+++ alibaba-rsocket-core/src/test/java/com/alibaba/rsocket/metadata/BearerTokenMetadataTest.java
@@ -1,7 +1,14 @@
 package com.alibaba.rsocket.metadata;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+import io.rsocket.metadata.security.AuthMetadataFlyweight;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Bearer token metadata test
@@ -17,5 +24,13 @@ public class BearerTokenMetadataTest {
         Assertions.assertEquals(token.length() + 1, tokenMetadata.getContent().capacity());
         BearerTokenMetadata tokenMetadata1 = BearerTokenMetadata.from(tokenMetadata.getContent());
         Assertions.assertEquals(tokenMetadata1.getBearerToken(), token);
+        ByteBuf byteBuf = AuthMetadataFlyweight.encodeBearerMetadata(ByteBufAllocator.DEFAULT, token.toCharArray());
+        assertThat(toArrayString(tokenMetadata.getContent())).isEqualTo(toArrayString(byteBuf));
+    }
+
+    private String toArrayString(ByteBuf byteBuf) {
+        byte[] bytes = new byte[byteBuf.capacity()];
+        byteBuf.readBytes(bytes);
+        return Arrays.toString(bytes);
     }
 }
