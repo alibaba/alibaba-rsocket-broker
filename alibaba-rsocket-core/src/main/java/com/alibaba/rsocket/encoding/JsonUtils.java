214cdb35ae5d748805c76ec756463cc391be5956
leijuan <jacky.chenlb@alibaba-inc.com>
2020-02-07 11:56:53 -0800

    Convert Exception to IOException and JsonProcessingException

--- alibaba-rsocket-core/src/main/java/com/alibaba/rsocket/encoding/JsonUtils.java
+++ alibaba-rsocket-core/src/main/java/com/alibaba/rsocket/encoding/JsonUtils.java
@@ -1,22 +1,21 @@
 package com.alibaba.rsocket.encoding;
 
 import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.core.util.MinimalPrettyPrinter;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import io.cloudevents.json.ZonedDateTimeDeserializer;
 import io.cloudevents.json.ZonedDateTimeSerializer;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufInputStream;
-import io.netty.buffer.Unpooled;
 
+import java.io.IOException;
 import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
 import java.time.ZonedDateTime;
 import java.util.List;
 
@@ -40,47 +39,35 @@ public class JsonUtils {
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     }
 
-    public static <T> T readJsonValue(ByteBuf byteBuf, Class<T> valueType) throws Exception {
+    public static <T> T readJsonValue(ByteBuf byteBuf, Class<T> valueType) throws IOException {
         return objectMapper.readValue((InputStream) new ByteBufInputStream(byteBuf), valueType);
     }
 
-    public static byte[] toJsonBytes(Object object) {
-        try {
-            return objectMapper.writeValueAsBytes(object);
-        } catch (Exception ignore) {
-            return new byte[]{};
-        }
+    public static byte[] toJsonBytes(Object object) throws JsonProcessingException {
+        return objectMapper.writeValueAsBytes(object);
     }
 
-    public static void updateJsonValue(ByteBuf byteBuf, Object object) throws Exception {
+    public static void updateJsonValue(ByteBuf byteBuf, Object object) throws IOException {
         objectMapper.readerForUpdating(object).readValue((InputStream) new ByteBufInputStream(byteBuf));
     }
 
-    public static String toJsonText(Object object) throws Exception {
+    public static String toJsonText(Object object) throws JsonProcessingException {
         return objectMapper.writeValueAsString(object);
     }
 
-    public static <T> T readJsonValue(String text, Class<T> valueType) throws Exception {
+    public static <T> T readJsonValue(String text, Class<T> valueType) throws JsonProcessingException {
         return objectMapper.readValue(text, valueType);
     }
 
-    public static <T> T convertValue(Object source, Class<T> valueType) throws Exception {
+    public static <T> T convertValue(Object source, Class<T> valueType) {
         return objectMapper.convertValue(source, valueType);
     }
 
-    public static ObjectNode convertToObjectNode(Object object) throws Exception {
-        return objectMapper.convertValue(object, ObjectNode.class);
-    }
-
-    public static void updateJsonValue(String text, Object object) throws Exception {
+    public static void updateJsonValue(String text, Object object) throws JsonProcessingException {
         objectMapper.readerForUpdating(object).readValue(text);
     }
 
-    public static Object[] readJsonArray(String text, Class<?>[] targetClasses) throws Exception {
-        return readJsonArray(Unpooled.wrappedBuffer(text.getBytes(StandardCharsets.UTF_8)), targetClasses);
-    }
-
-    public static Object[] readJsonArray(ByteBuf byteBuf, Class<?>[] targetClasses) throws Exception {
+    public static Object[] readJsonArray(ByteBuf byteBuf, Class<?>[] targetClasses) throws IOException {
         Object[] targets = new Object[targetClasses.length];
         List<JsonNode> jsonNodes = objectMapper.readValue(new ByteBufInputStream(byteBuf), new TypeReference<List<JsonNode>>() {
         });
