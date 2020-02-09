e4287b968e2530806bf44030457b163778e24bcf
leijuan <jacky.chenlb@alibaba-inc.com>
2020-02-09 14:09:46 -0800

    introduce faker

--- example/rsocket-responder/src/main/java/com/alibaba/spring/boot/rsocket/demo/AccountServiceImpl.java
+++ example/rsocket-responder/src/main/java/com/alibaba/spring/boot/rsocket/demo/AccountServiceImpl.java
@@ -3,6 +3,7 @@ package com.alibaba.spring.boot.rsocket.demo;
 import com.alibaba.account.Account;
 import com.alibaba.account.AccountService;
 import com.alibaba.spring.boot.rsocket.RSocketService;
+import com.github.javafaker.Faker;
 import com.google.protobuf.Int32Value;
 import reactor.core.publisher.Mono;
 
@@ -13,13 +14,14 @@ import reactor.core.publisher.Mono;
  */
 @RSocketService(serviceInterface = AccountService.class, encoding = "protobuf")
 public class AccountServiceImpl implements AccountService {
+    private Faker faker = new Faker();
+
     @Override
     public Mono<Account> findById(Int32Value id) {
-        return Mono.fromCallable(() -> Account.newBuilder()
-                .setId(id.getValue())
-                .setEmail("demo@demo.com")
-                .setPhone("1860000000")
-                .build()
-        );
+        return Mono.just(Account.newBuilder().setId(id.getValue())
+                .setEmail(faker.internet().emailAddress())
+                .setPhone(faker.phoneNumber().cellPhone())
+                .setNick(faker.name().name())
+                .build());
     }
 }
