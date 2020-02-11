301adf26fc57a0990086574fefc7e1ad1db24dd5
leijuan <jacky.chenlb@alibaba-inc.com>
2020-02-10 17:02:06 -0800

    add cloudEventToMetadataPushPayload

--- alibaba-rsocket-core/src/main/java/com/alibaba/rsocket/cloudevents/CloudEventRSocket.java
+++ alibaba-rsocket-core/src/main/java/com/alibaba/rsocket/cloudevents/CloudEventRSocket.java
@@ -1,7 +1,11 @@
 package com.alibaba.rsocket.cloudevents;
 
+import io.cloudevents.json.Json;
 import io.cloudevents.v1.CloudEventImpl;
+import io.netty.buffer.Unpooled;
+import io.rsocket.Payload;
 import io.rsocket.RSocket;
+import io.rsocket.util.ByteBufPayload;
 import reactor.core.publisher.Mono;
 
 /**
@@ -12,4 +16,8 @@ import reactor.core.publisher.Mono;
 public interface CloudEventRSocket extends RSocket {
 
     Mono<Void> fireCloudEvent(CloudEventImpl<?> cloudEvent);
+
+    default Payload cloudEventToMetadataPushPayload(CloudEventImpl<?> cloudEvent) {
+        return ByteBufPayload.create(Unpooled.EMPTY_BUFFER, Unpooled.wrappedBuffer(Json.binaryEncode(cloudEvent)));
+    }
 }
