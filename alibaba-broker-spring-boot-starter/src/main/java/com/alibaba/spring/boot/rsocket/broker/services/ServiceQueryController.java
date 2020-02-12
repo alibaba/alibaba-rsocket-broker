d2471190aca4e5fa5890d6cd75a6fc26710dbdcc
leijuan <jacky.chenlb@alibaba-inc.com>
2020-02-12 15:17:11 -0800

    polishing

--- alibaba-broker-spring-boot-starter/src/main/java/com/alibaba/spring/boot/rsocket/broker/services/ServiceQueryController.java
+++ alibaba-broker-spring-boot-starter/src/main/java/com/alibaba/spring/boot/rsocket/broker/services/ServiceQueryController.java
@@ -1,6 +1,5 @@
 package com.alibaba.spring.boot.rsocket.broker.services;
 
-import com.alibaba.rsocket.metadata.AppMetadata;
 import com.alibaba.spring.boot.rsocket.broker.responder.RSocketBrokerHandlerRegistry;
 import com.alibaba.spring.boot.rsocket.broker.route.ServiceRoutingSelector;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,11 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 /**
  * service query controller
@@ -32,20 +28,16 @@ public class ServiceQueryController {
     @GetMapping("/{serviceName}")
     public Flux<Map<String, Object>> query(@PathVariable(name = "serviceName") String serviceName) {
         return Flux.fromIterable(routingSelector.findAllServices())
-                .filter(s -> s.contains(":" + serviceName + ":"))
-                .map(s -> {
+                .filter(locator -> locator.getService().equals(serviceName))
+                .map(locator -> {
                     Map<String, Object> serviceInfo = new HashMap<>();
-                    //todo 调整逻辑
-                    Set<String> appInstances = Collections.emptySet(); //serviceRoutingSelector.getAppInstances(s);
-                    Set<Map<String, Object>> appList = appInstances.stream()
-                            .map(appId -> handlerRegistry.findByUUID(appId))
-                            .map(handler -> {
-                                AppMetadata appMetadata = handler.getAppMetadata();
-                                Map<String, Object> app = new HashMap<>();
-                                app.put("ip", appMetadata.getIp());
-                                return app;
-                            }).collect(Collectors.toSet());
-                    serviceInfo.put("apps", appList);
+                    serviceInfo.put("count", routingSelector.getInstanceCount(locator.getId()));
+                    if (locator.getGroup() != null) {
+                        serviceInfo.put("group", locator.getGroup());
+                    }
+                    if (locator.getVersion() != null) {
+                        serviceInfo.put("version", locator.getVersion());
+                    }
                     return serviceInfo;
                 });
     }
