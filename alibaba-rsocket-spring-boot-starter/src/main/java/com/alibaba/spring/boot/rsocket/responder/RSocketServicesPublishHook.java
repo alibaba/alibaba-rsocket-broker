d959ba923b1a338a655d3601f117092a68b72353
leijuan <jacky.chenlb@alibaba-inc.com>
2020-02-12 19:45:23 -0800

    add Info

--- alibaba-rsocket-spring-boot-starter/src/main/java/com/alibaba/spring/boot/rsocket/responder/RSocketServicesPublishHook.java
+++ alibaba-rsocket-spring-boot-starter/src/main/java/com/alibaba/spring/boot/rsocket/responder/RSocketServicesPublishHook.java
@@ -5,12 +5,14 @@ import com.alibaba.rsocket.RSocketRequesterSupport;
 import com.alibaba.rsocket.ServiceLocator;
 import com.alibaba.rsocket.events.AppStatusEvent;
 import com.alibaba.rsocket.events.ServicesExposedEvent;
+import com.alibaba.rsocket.loadbalance.LoadBalancedRSocket;
 import com.alibaba.rsocket.upstream.UpstreamCluster;
 import com.alibaba.rsocket.upstream.UpstreamManager;
 import com.alibaba.spring.boot.rsocket.RSocketProperties;
-import io.cloudevents.CloudEvent;
 import io.cloudevents.v1.CloudEventBuilder;
 import io.cloudevents.v1.CloudEventImpl;
+import io.rsocket.metadata.WellKnownMimeType;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,8 +42,8 @@ public class RSocketServicesPublishHook implements ApplicationListener<Applicati
     private RSocketRequesterSupport rsocketRequesterSupport;
 
     @Override
-    public void onApplicationEvent(ApplicationReadyEvent event) {
-        UpstreamCluster brokerCluster = upstreamManager.findClusterByServiceId("*");
+    public void onApplicationEvent(@NotNull ApplicationReadyEvent applicationReadyEvent) {
+        UpstreamCluster brokerCluster = upstreamManager.findBroker();
         if (brokerCluster == null) return;
         //rsocket broker cluster logic
         CloudEventImpl<AppStatusEvent> appStatusEventCloudEvent = CloudEventBuilder.<AppStatusEvent>builder()
@@ -49,14 +51,17 @@ public class RSocketServicesPublishHook implements ApplicationListener<Applicati
                 .withTime(ZonedDateTime.now())
                 .withSource(URI.create("app://" + RSocketAppContext.ID))
                 .withType(AppStatusEvent.class.getCanonicalName())
-                .withDataContentType("application/json")
+                .withDataContentType(WellKnownMimeType.APPLICATION_JSON.getString())
                 .withData(new AppStatusEvent(RSocketAppContext.ID, AppStatusEvent.STATUS_SERVING))
                 .build();
-        brokerCluster.fireCloudEventToUpstreamAll(appStatusEventCloudEvent).doOnSuccess(aVoid -> log.info("App status: Serving")).subscribe();
+        LoadBalancedRSocket loadBalancedRSocket = brokerCluster.getLoadBalancedRSocket();
+        loadBalancedRSocket.fireCloudEventToUpstreamAll(appStatusEventCloudEvent).doOnSuccess(aVoid -> log.info("App status: Serving")).subscribe();
         CloudEventImpl<ServicesExposedEvent> servicesExposedEventCloudEvent = rsocketRequesterSupport.servicesExposedEvent().get();
         if (servicesExposedEventCloudEvent != null) {
-            brokerCluster.fireCloudEventToUpstreamAll(servicesExposedEventCloudEvent).doOnSuccess(aVoid -> {
-                log.info("Services exposed on Broker: " + rsocketRequesterSupport.exposedServices().get().stream().map(ServiceLocator::toString).collect(Collectors.joining(",")));
+            loadBalancedRSocket.fireCloudEventToUpstreamAll(servicesExposedEventCloudEvent).doOnSuccess(aVoid -> {
+                String exposedServices = rsocketRequesterSupport.exposedServices().get().stream().map(ServiceLocator::getGsv).collect(Collectors.joining(","));
+                String brokers = String.join(",", loadBalancedRSocket.getActiveSockets().keySet());
+                log.info("Services(" + exposedServices + ") published on Brokers(" + brokers + ")!");
             }).subscribe();
         }
     }
