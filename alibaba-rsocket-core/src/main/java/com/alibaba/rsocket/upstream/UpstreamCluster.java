3f5f10ce822e63c27004113cdd3dea1c462e50d9
leijuan <jacky.chenlb@alibaba-inc.com>
2020-02-12 19:26:22 -0800

    remove AbstractRSocket extend

--- alibaba-rsocket-core/src/main/java/com/alibaba/rsocket/upstream/UpstreamCluster.java
+++ alibaba-rsocket-core/src/main/java/com/alibaba/rsocket/upstream/UpstreamCluster.java
@@ -3,17 +3,10 @@ package com.alibaba.rsocket.upstream;
 import com.alibaba.rsocket.Initializable;
 import com.alibaba.rsocket.RSocketRequesterSupport;
 import com.alibaba.rsocket.ServiceLocator;
-import com.alibaba.rsocket.cloudevents.CloudEventRSocket;
 import com.alibaba.rsocket.loadbalance.LoadBalancedRSocket;
 import com.alibaba.rsocket.observability.RsocketErrorCode;
-import io.cloudevents.v1.CloudEventImpl;
-import io.rsocket.AbstractRSocket;
-import io.rsocket.Payload;
-import org.reactivestreams.Publisher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 import reactor.core.publisher.ReplayProcessor;
 
 import java.io.Closeable;
@@ -26,7 +19,7 @@ import java.util.List;
  *
  * @author leijuan
  */
-public class UpstreamCluster extends AbstractRSocket implements CloudEventRSocket, Initializable, Closeable {
+public class UpstreamCluster implements Initializable, Closeable {
     private Logger log = LoggerFactory.getLogger(UpstreamCluster.class);
     private String group;
     private String serviceName;
@@ -40,7 +33,7 @@ public class UpstreamCluster extends AbstractRSocket implements CloudEventRSocke
      */
     private ReplayProcessor<Collection<String>> urisProcessor = ReplayProcessor.cacheLast();
     /**
-     * load balanced RSocket: load balance and auto reconnect
+     * load balanced RSocket to connect service provider or broker instances
      */
     private LoadBalancedRSocket loadBalancedRSocket;
     /**
@@ -111,65 +104,6 @@ public class UpstreamCluster extends AbstractRSocket implements CloudEventRSocke
         return loadBalancedRSocket;
     }
 
-    //------------- RSocket methods Start------------//
-
-    @Override
-    public Mono<Payload> requestResponse(Payload payload) {
-        return loadBalancedRSocket.requestResponse(payload);
-    }
-
-    @Override
-    public Mono<Void> fireAndForget(Payload payload) {
-        return loadBalancedRSocket.fireAndForget(payload);
-    }
-
-    @Override
-    public Flux<Payload> requestStream(Payload payload) {
-        return loadBalancedRSocket.requestStream(payload);
-    }
-
-    @Override
-    public Flux<Payload> requestChannel(Publisher<Payload> payloads) {
-        return loadBalancedRSocket.requestChannel(payloads);
-    }
-
-    @Override
-    public Mono<Void> fireCloudEvent(CloudEventImpl<?> cloudEvent) {
-        try {
-            return metadataPush(cloudEventToMetadataPushPayload(cloudEvent));
-        } catch (Exception e) {
-            return Mono.empty();
-        }
-    }
-
-    @Override
-    public Mono<Void> metadataPush(Payload payload) {
-        return loadBalancedRSocket.metadataPush(payload);
-    }
-
-    @Override
-    public double availability() {
-        return loadBalancedRSocket.availability();
-    }
-
-    @Override
-    public void dispose() {
-        super.dispose();
-        close();
-    }
-
-    @Override
-    public boolean isDisposed() {
-        return super.isDisposed();
-    }
-
-    @Override
-    public Mono<Void> onClose() {
-        return super.onClose();
-    }
-
-    //------------- RSocket methods End------------//
-
     @Override
     public void close() {
         try {
@@ -190,21 +124,4 @@ public class UpstreamCluster extends AbstractRSocket implements CloudEventRSocke
         }
     }
 
-    /**
-     * fire cloud event to upstream all nodes
-     *
-     * @param cloudEvent cloud event
-     * @return void
-     */
-    public Mono<Void> fireCloudEventToUpstreamAll(CloudEventImpl<?> cloudEvent) {
-        try {
-            Payload payload = cloudEventToMetadataPushPayload(cloudEvent);
-            return Flux.fromIterable(loadBalancedRSocket.getActiveSockets().values())
-                    .flatMap(rSocket -> rSocket.metadataPush(payload))
-                    .doOnError(throwable -> log.error("Failed to fire event to upstream", throwable))
-                    .then();
-        } catch (Exception e) {
-            return Mono.error(e);
-        }
-    }
 }
