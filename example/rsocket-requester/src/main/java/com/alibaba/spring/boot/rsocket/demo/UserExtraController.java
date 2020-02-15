cf277d7253feedf14300f1f1ba64c2b4abb592eb
leijuan <jacky.chenlb@alibaba-inc.com>
2020-02-14 20:48:40 -0800

    user extra controller

---
+++ example/rsocket-requester/src/main/java/com/alibaba/spring/boot/rsocket/demo/UserExtraController.java
@@ -0,0 +1,30 @@
+package com.alibaba.spring.boot.rsocket.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.MimeTypeUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.nio.ByteBuffer;
+
+/**
+ * user extra controller
+ *
+ * @author leijuan
+ */
+@RestController
+@RequestMapping("/user/extra")
+public class UserExtraController {
+    @Autowired
+    private UserServiceExtra userServiceExtra;
+
+
+    @GetMapping(value = "/{id}", produces = MimeTypeUtils.APPLICATION_JSON_VALUE)
+    public Mono<ByteBuffer> user(@PathVariable Integer id) {
+        return userServiceExtra.findById(id);
+    }
+
+}
