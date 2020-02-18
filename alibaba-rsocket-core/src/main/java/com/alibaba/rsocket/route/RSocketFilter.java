35db791af6d69c2ea7d8dec33291a1441e466888
leijuan <jacky.chenlb@alibaba-inc.com>
2020-02-18 14:41:32 -0800

    add enable filter

--- alibaba-rsocket-core/src/main/java/com/alibaba/rsocket/route/RSocketFilter.java
+++ alibaba-rsocket-core/src/main/java/com/alibaba/rsocket/route/RSocketFilter.java
@@ -8,35 +8,45 @@ import reactor.core.publisher.Mono;
  *
  * @author leijuan
  */
-public interface RSocketFilter {
+public abstract class RSocketFilter {
+    private boolean enabled = true;
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
     /**
      * filter or not
      *
      * @param exchange request exchange
      * @return filter required or not
      */
-    Mono<Boolean> shouldFilter(RSocketExchange exchange);
+    public abstract Mono<Boolean> shouldFilter(RSocketExchange exchange);
 
     /**
      * run filter logic, no block code
      *
      * @param exchange request exchange
      */
-    Mono<Void> run(RSocketExchange exchange);
+    public abstract Mono<Void> run(RSocketExchange exchange);
 
     /**
      * filter name or description
      *
      * @return filter name
      */
-    String name();
+   public abstract String name();
 
     /**
      * refresh filter logic dynamically
      *
      * @param properties properties configuration
      */
-    default void refresh(String properties) {
+    public void refresh(String properties) {
 
     }
 }
