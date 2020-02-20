167bae7036ffab8f7a0bdbcdd41a59472b61980d
leijuan <jacky.chenlb@alibaba-inc.com>
2020-02-19 21:03:05 -0800

    move DisposableBean logic to gossip broker manager

--- alibaba-broker-server/src/main/java/com/alibaba/rsocket/broker/AlibabaRSocketBrokerServer.java
+++ alibaba-broker-server/src/main/java/com/alibaba/rsocket/broker/AlibabaRSocketBrokerServer.java
@@ -3,7 +3,6 @@ package com.alibaba.rsocket.broker;
 import com.alibaba.rsocket.broker.filters.CanaryFilter;
 import com.alibaba.rsocket.route.RSocketFilter;
 import com.alibaba.spring.boot.rsocket.broker.cluster.RSocketBrokerManager;
-import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,20 +17,12 @@ import java.time.LocalDateTime;
  * @author leijuan
  */
 @SpringBootApplication
-public class AlibabaRSocketBrokerServer implements DisposableBean {
+public class AlibabaRSocketBrokerServer {
     public static final LocalDateTime STARTED_AT = LocalDateTime.now();
-
-    @Autowired
-    private RSocketBrokerManager brokerManager;
-
+    
     public static void main(String[] args) {
         //BlockHound.install();
         SpringApplication.run(AlibabaRSocketBrokerServer.class, args);
-    }
-
-    @Override
-    public void destroy() throws Exception {
-        brokerManager.stopLocalBroker();
     }
 
     /**
