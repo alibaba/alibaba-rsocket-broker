c96493f2ecba1d67765b1e8d10b8a0894fc8dba4
leijuan <jacky.chenlb@alibaba-inc.com>
2020-02-20 12:23:09 -0800

    introduce ByteBuf for performance

--- alibaba-rsocket-core/src/main/java/com/alibaba/rsocket/cloudevents/CloudEventRSocket.java
+++ alibaba-rsocket-core/src/main/java/com/alibaba/rsocket/cloudevents/CloudEventRSocket.java
@@ -1,14 +1,22 @@
 package com.alibaba.rsocket.cloudevents;
 
+import com.alibaba.rsocket.encoding.EncodingException;
+import com.alibaba.rsocket.encoding.JsonUtils;
+import com.alibaba.rsocket.observability.RsocketErrorCode;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import io.cloudevents.json.Json;
 import io.cloudevents.v1.CloudEventImpl;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufOutputStream;
+import io.netty.buffer.PooledByteBufAllocator;
 import io.netty.buffer.Unpooled;
+import io.netty.util.ReferenceCountUtil;
 import io.rsocket.Payload;
 import io.rsocket.RSocket;
 import io.rsocket.util.ByteBufPayload;
 import reactor.core.publisher.Mono;
+
+import java.io.OutputStream;
 
 /**
  * RSocket with CloudEvents support
@@ -22,6 +30,14 @@ public interface CloudEventRSocket extends RSocket {
     Mono<Void> fireCloudEvent(CloudEventImpl<?> cloudEvent);
 
     default Payload cloudEventToMetadataPushPayload(CloudEventImpl<?> cloudEvent) {
-        return ByteBufPayload.create(Unpooled.EMPTY_BUFFER, Unpooled.wrappedBuffer(Json.binaryEncode(cloudEvent)));
+        ByteBuf byteBuf = PooledByteBufAllocator.DEFAULT.buffer();
+        try {
+            ByteBufOutputStream bos = new ByteBufOutputStream(byteBuf);
+            JsonUtils.objectMapper.writeValue((OutputStream) bos, cloudEvent);
+            return ByteBufPayload.create(Unpooled.EMPTY_BUFFER, byteBuf);
+        } catch (Exception e) {
+            ReferenceCountUtil.safeRelease(byteBuf);
+            throw new EncodingException(RsocketErrorCode.message("RST-700500", "CloudEventImpl", "ByteBuf"), e);
+        }
     }
 }
