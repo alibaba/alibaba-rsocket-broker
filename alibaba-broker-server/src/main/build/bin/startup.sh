8b74a02af7be2b368e8f4c2a5dd02fd1342eb234
leijuan <jacky.chenlb@alibaba-inc.com>
2020-02-25 14:31:23 -0800

    polishing

--- alibaba-broker-server/src/main/build/bin/startup.sh
+++ alibaba-broker-server/src/main/build/bin/startup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 PRGDIR=$(dirname "$0")
 JAR_FILE=alibaba-rsocket-broker.jar
-mkdir -p logs
+
+if [ ! -e "$PRGDIR/logs" ] ; then
+   mkdir -p "$PRGDIR/logs"
+fi
+
 nohup java ${JAVA_OPTS} -jar "${PRGDIR}"/lib/${JAR_FILE} > "${PRGDIR}"/logs/application.log 2>&1 &
 echo $! > "${PRGDIR}"/app.pid
-echo "RSocket Server started."
+echo "Alibaba RSocket Server started."
