0abb36c806ddfbe6b34ad336e503eed9abbcb3d1
leijuan <jacky.chenlb@alibaba-inc.com>
2020-02-26 16:04:48 -0800

    Byte buf builder

---
+++ example/user-service-api/src/main/java/com/alibaba/ByteBufBuilder.java
@@ -0,0 +1,66 @@
+package com.alibaba;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+/**
+ * Byte buf builder
+ *
+ * @author leijuan
+ */
+public class ByteBufBuilder {
+    ByteBuf buf = Unpooled.buffer();
+
+    public static ByteBufBuilder builder() {
+        return new ByteBufBuilder();
+    }
+
+    public ByteBufBuilder value(byte value) {
+        buf.writeByte(value);
+        return this;
+    }
+
+    public ByteBufBuilder value(int value) {
+        buf.writeInt(value);
+        return this;
+    }
+
+    public ByteBufBuilder value(long value) {
+        buf.writeLong(value);
+        return this;
+    }
+
+    public ByteBufBuilder value(double value) {
+        buf.writeDouble(value);
+        return this;
+    }
+
+    public ByteBufBuilder value(boolean value) {
+        buf.writeByte(value ? 1 : 0);
+        return this;
+    }
+
+    public ByteBufBuilder value(String value) {
+        if (value == null) {
+            buf.writeInt(0);
+        } else {
+            byte[] bytes = value.getBytes(StandardCharsets.UTF_8);
+            buf.writeInt(bytes.length);
+            buf.writeBytes(bytes);
+        }
+        return this;
+    }
+
+    public ByteBufBuilder value(Date value) {
+        buf.writeLong(value.getTime());
+        return this;
+    }
+
+    public ByteBuf build() {
+        return this.buf;
+    }
+
+}
