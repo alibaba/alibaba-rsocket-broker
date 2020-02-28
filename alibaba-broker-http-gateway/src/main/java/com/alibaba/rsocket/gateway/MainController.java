3669b07f4c53a42acae5c0805cef7eabc7fba5a3
leijuan <jacky.chenlb@alibaba-inc.com>
2020-02-28 12:33:34 -0800

    Introduce ByteBuf for performance

--- alibaba-broker-http-gateway/src/main/java/com/alibaba/rsocket/gateway/MainController.java
+++ alibaba-broker-http-gateway/src/main/java/com/alibaba/rsocket/gateway/MainController.java
@@ -6,15 +6,12 @@ import com.alibaba.rsocket.metadata.RSocketCompositeMetadata;
 import com.alibaba.rsocket.metadata.RSocketMimeType;
 import com.alibaba.rsocket.upstream.UpstreamManager;
 import io.netty.buffer.ByteBuf;
-import io.netty.buffer.Unpooled;
 import io.rsocket.RSocket;
-import io.rsocket.util.DefaultPayload;
+import io.rsocket.util.ByteBufPayload;
 import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
-
-import java.nio.ByteBuffer;
 
 import static io.netty.buffer.Unpooled.EMPTY_BUFFER;
 
@@ -32,25 +29,25 @@ public class MainController {
         rsocket = upstreamManager.findBroker().getLoadBalancedRSocket();
     }
 
-    @RequestMapping("/{serviceName}/{method}")
-    public Mono<ResponseEntity<ByteBuffer>> handle(@PathVariable("serviceName") String serviceName,
-                                                   @PathVariable("method") String method,
-                                                   @RequestParam(name = "group", required = false, defaultValue = "") String group,
-                                                   @RequestParam(name = "version", required = false, defaultValue = "") String version,
-                                                   @RequestBody(required = false) byte[] body,
-                                                   @RequestHeader(name = "Authorization", required = false, defaultValue = "") String authorization) {
+    @RequestMapping(value = "/{serviceName}/{method}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public Mono<ResponseEntity<ByteBuf>> handle(@PathVariable("serviceName") String serviceName,
+                                                @PathVariable("method") String method,
+                                                @RequestParam(name = "group", required = false, defaultValue = "") String group,
+                                                @RequestParam(name = "version", required = false, defaultValue = "") String version,
+                                                @RequestBody(required = false) ByteBuf body,
+                                                @RequestHeader(name = "Authorization", required = false, defaultValue = "") String authorization) {
         //todo authorization: JWT, SAML etc
         // please add auth code here
         try {
             GSVRoutingMetadata routingMetadata = new GSVRoutingMetadata(group, serviceName, method, version);
             RSocketCompositeMetadata compositeMetadata = RSocketCompositeMetadata.from(routingMetadata, jsonMetaEncoding);
-            ByteBuf bodyBuf = body == null ? EMPTY_BUFFER : Unpooled.wrappedBuffer(body);
-            return rsocket.requestResponse(DefaultPayload.create(bodyBuf, compositeMetadata.getContent()))
+            ByteBuf bodyBuf = body == null ? EMPTY_BUFFER : body;
+            return rsocket.requestResponse(ByteBufPayload.create(bodyBuf, compositeMetadata.getContent()))
                     .map(payload -> {
                         HttpHeaders headers = new HttpHeaders();
                         headers.setContentType(MediaType.APPLICATION_JSON);
                         headers.setCacheControl(CacheControl.noCache().getHeaderValue());
-                        return new ResponseEntity<>(payload.getData(), headers, HttpStatus.OK);
+                        return new ResponseEntity<>(payload.data(), headers, HttpStatus.OK);
                     });
         } catch (Exception e) {
             return Mono.error(e);
