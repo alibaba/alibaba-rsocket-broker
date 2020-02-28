333a7b1908ff38d0c90e347f2b7e7087f74bd960
leijuan <jacky.chenlb@alibaba-inc.com>
2020-02-28 15:02:34 -0800

    remove filter bean. use @Component instead

--- alibaba-broker-server/src/main/java/com/alibaba/rsocket/broker/AlibabaRSocketBrokerServer.java
+++ alibaba-broker-server/src/main/java/com/alibaba/rsocket/broker/AlibabaRSocketBrokerServer.java
@@ -1,12 +1,9 @@
 package com.alibaba.rsocket.broker;
 
-import com.alibaba.rsocket.broker.filters.CanaryFilter;
-import com.alibaba.rsocket.route.RSocketFilter;
 import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.Bean;
 
 import java.time.LocalDateTime;
@@ -21,23 +18,11 @@ public class AlibabaRSocketBrokerServer {
     public static final LocalDateTime STARTED_AT = LocalDateTime.now();
 
     public static void main(String[] args) {
-        //BlockHound.install();
         SpringApplication.run(AlibabaRSocketBrokerServer.class, args);
     }
 
     @Bean
     MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
         return registry -> registry.config().commonTags("application", "alibaba-rsocket-broker");
-    }
-
-    /**
-     * canary RSocket filter
-     *
-     * @return CanaryFilter
-     */
-    @Bean
-    @ConditionalOnExpression(value = "false")
-    public RSocketFilter canaryRSocketFilter() {
-        return new CanaryFilter();
     }
 }
