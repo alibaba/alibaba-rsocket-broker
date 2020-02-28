facad8fc3855aec23bde3ade3827a736b6b593df
leijuan <jacky.chenlb@alibaba-inc.com>
2020-02-28 12:33:11 -0800

    add ByteBufEncoder and ByteBufDecoder

--- alibaba-broker-http-gateway/src/main/java/com/alibaba/rsocket/gateway/RSocketBrokerHttpGatewayApp.java
+++ alibaba-broker-http-gateway/src/main/java/com/alibaba/rsocket/gateway/RSocketBrokerHttpGatewayApp.java
@@ -1,7 +1,13 @@
 package com.alibaba.rsocket.gateway;
 
+import com.alibaba.rsocket.gateway.converter.ByteBufDecoder;
+import com.alibaba.rsocket.gateway.converter.ByteBufEncoder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.codec.DecoderHttpMessageReader;
+import org.springframework.http.codec.EncoderHttpMessageWriter;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 /**
  * RSocket Broker HTTP Gateway App
@@ -9,8 +15,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author leijuan
  */
 @SpringBootApplication
-public class RSocketBrokerHttpGatewayApp {
+public class RSocketBrokerHttpGatewayApp implements WebFluxConfigurer {
     public static void main(String[] args) {
         SpringApplication.run(RSocketBrokerHttpGatewayApp.class, args);
+    }
+
+    @Override
+    public void configureHttpMessageCodecs(ServerCodecConfigurer configurer) {
+        configurer.customCodecs().register(new EncoderHttpMessageWriter<>(new ByteBufEncoder()));
+        configurer.customCodecs().register(new DecoderHttpMessageReader<>(new ByteBufDecoder()));
     }
 }
