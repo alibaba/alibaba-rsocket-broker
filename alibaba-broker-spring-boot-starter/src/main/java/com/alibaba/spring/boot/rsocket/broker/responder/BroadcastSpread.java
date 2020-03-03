4400a750d923d4b4b1b557f8d188434678580629
leijuan <jacky.chenlb@alibaba-inc.com>
2020-03-03 14:58:12 -0800

    add buildMapCloudEvent()

--- alibaba-broker-spring-boot-starter/src/main/java/com/alibaba/spring/boot/rsocket/broker/responder/BroadcastSpread.java
+++ alibaba-broker-spring-boot-starter/src/main/java/com/alibaba/spring/boot/rsocket/broker/responder/BroadcastSpread.java
@@ -1,7 +1,15 @@
 package com.alibaba.spring.boot.rsocket.broker.responder;
 
+import com.alibaba.rsocket.RSocketAppContext;
+import io.cloudevents.v1.CloudEventBuilder;
 import io.cloudevents.v1.CloudEventImpl;
+import org.jetbrains.annotations.NotNull;
 import reactor.core.publisher.Mono;
+
+import java.net.URI;
+import java.time.ZonedDateTime;
+import java.util.Map;
+import java.util.UUID;
 
 /**
  * broadcast spread for cloudevents
@@ -14,4 +22,15 @@ public interface BroadcastSpread {
 
     Mono<Void> broadcast(String appName, final CloudEventImpl cloudEvent);
 
+    default CloudEventImpl<Map<String, Object>> buildMapCloudEvent(@NotNull String type, @NotNull String subject, @NotNull Map<String, Object> data) {
+        return CloudEventBuilder.<Map<String, Object>>builder()
+                .withId(UUID.randomUUID().toString())
+                .withSource(URI.create("broker://" + RSocketAppContext.ID))
+                .withType(type)
+                .withTime(ZonedDateTime.now())
+                .withDataContentType("application/json")
+                .withData(data)
+                .withSubject(subject)
+                .build();
+    }
 }
