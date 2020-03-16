7a64e855fde5cb750a837438fe41f88bcb8e95a6
leijuan <jacky.chenlb@alibaba-inc.com>
2020-03-16 16:19:00 -0700

    Hessian test

---
+++ alibaba-rsocket-core/src/test/java/com/alibaba/rsocket/encoding/hessian/io/HessianTest.java
@@ -0,0 +1,46 @@
+package com.alibaba.rsocket.encoding.hessian.io;
+
+import com.caucho.hessian.io.HessianSerializerInput;
+import com.caucho.hessian.io.HessianSerializerOutput;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.time.*;
+import java.util.Locale;
+import java.util.Optional;
+
+/**
+ * Hessian test
+ *
+ * @author leijuan
+ */
+public class HessianTest {
+
+    @Test
+    public void testSerializers() throws Exception {
+        HessianObject hessianObject = new HessianObject();
+        hessianObject.setLocalDate(LocalDate.now());
+        hessianObject.setLocalDateTime(LocalDateTime.now());
+        hessianObject.setLocalTime(LocalTime.now());
+        hessianObject.setDuration(Duration.ofSeconds(111));
+        hessianObject.setInstant(Instant.now());
+        hessianObject.setPeriod(Period.of(1, 3, 16));
+        hessianObject.setMonthDay(MonthDay.of(1, 2));
+        hessianObject.setYearMonth(YearMonth.of(3, 2));
+        hessianObject.setOptional(Optional.of("hello world"));
+        hessianObject.setLocale(Locale.CHINESE);
+        hessianObject.setOffsetTime(OffsetTime.of(1, 2, 13, 11, ZoneOffset.UTC));
+        hessianObject.setOffsetDateTime(OffsetDateTime.now());
+        hessianObject.setZonedDateTime(ZonedDateTime.now());
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        HessianSerializerOutput output = new HessianSerializerOutput(bos);
+        output.writeObject(hessianObject);
+        output.flush();
+        ByteArrayInputStream bis = new ByteArrayInputStream(bos.toByteArray());
+        HessianSerializerInput input = new HessianSerializerInput(bis);
+        HessianObject hessianObject2 = (HessianObject) input.readObject();
+        Assertions.assertThat(hessianObject).isEqualToComparingFieldByField(hessianObject2);
+    }
+}
