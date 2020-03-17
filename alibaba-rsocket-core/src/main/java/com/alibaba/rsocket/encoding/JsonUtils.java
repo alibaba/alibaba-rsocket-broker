b7839de6ebab04b8b8bb613eb14c0a017f922558
leijuan <jacky.chenlb@alibaba-inc.com>
2020-03-17 13:16:54 -0700

    introduce objectMapper.findAndRegisterModules() to load customized modules

--- alibaba-rsocket-core/src/main/java/com/alibaba/rsocket/encoding/JsonUtils.java
+++ alibaba-rsocket-core/src/main/java/com/alibaba/rsocket/encoding/JsonUtils.java
@@ -1,5 +1,6 @@
 package com.alibaba.rsocket.encoding;
 
+import com.alibaba.rsocket.encoding.json.CloudEventsModule;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -7,10 +8,6 @@ import com.fasterxml.jackson.core.util.MinimalPrettyPrinter;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.module.SimpleModule;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
-import io.cloudevents.json.ZonedDateTimeDeserializer;
-import io.cloudevents.json.ZonedDateTimeSerializer;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufInputStream;
 import io.netty.buffer.ByteBufOutputStream;
@@ -20,7 +17,6 @@ import io.netty.util.ReferenceCountUtil;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.time.ZonedDateTime;
 import java.util.List;
 
 /**
@@ -32,12 +28,8 @@ public class JsonUtils {
     public static final ObjectMapper objectMapper = new ObjectMapper();
 
     static {
-        // add Jackson datatype for ZonedDateTime
-        objectMapper.registerModule(new Jdk8Module());
-        final SimpleModule module = new SimpleModule();
-        module.addSerializer(ZonedDateTime.class, new ZonedDateTimeSerializer());
-        module.addDeserializer(ZonedDateTime.class, new ZonedDateTimeDeserializer());
-        objectMapper.registerModule(module);
+        objectMapper.findAndRegisterModules();
+        objectMapper.registerModule(new CloudEventsModule());
         objectMapper.setDefaultPrettyPrinter(new MinimalPrettyPrinter());
         objectMapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
