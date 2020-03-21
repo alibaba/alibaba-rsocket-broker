6443369f24461f1b017357cc74f58d79403b06b2
leijuan <jacky.chenlb@alibaba-inc.com>
2020-03-21 13:07:31 -0700

    RSocket gRPC Support

---
+++ alibaba-broker-grpc-gateway/src/main/java/com/alibaba/rsocket/gateway/grpc/impl/RSocketGrpcSupport.java
@@ -0,0 +1,38 @@
+package com.alibaba.rsocket.gateway.grpc.impl;
+
+import com.alibaba.rsocket.gateway.grpc.PayloadUtils;
+import io.rsocket.Payload;
+import io.rsocket.RSocket;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+/**
+ * RSocket gRPC Support
+ *
+ * @author leijuan
+ */
+public interface RSocketGrpcSupport {
+
+    default <T> Mono<T> rsocketRpc(RSocket rsocket, Payload requestPayload, final Class<T> responseClass) {
+        return rsocket.requestResponse(requestPayload)
+                .handle((payload, sink) -> {
+                    try {
+                        sink.next(PayloadUtils.payloadToResponseObject(payload, responseClass));
+                    } catch (Exception e) {
+                        sink.error(e);
+                    }
+                });
+    }
+
+    default <T> Flux<T> rsocketStream(RSocket rsocket, Payload requestPayload, final Class<T> responseClass) {
+        return rsocket.requestStream(requestPayload)
+                .handle((payload, sink) -> {
+                    try {
+                        sink.next(PayloadUtils.payloadToResponseObject(payload, responseClass));
+                    } catch (Exception e) {
+                        sink.error(e);
+                    }
+                });
+    }
+
+}
