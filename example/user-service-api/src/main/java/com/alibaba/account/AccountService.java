ace6477584423fbfe3e0597e342098919af4c9d2
leijuan <jacky.chenlb@alibaba-inc.com>
2020-03-21 13:24:49 -0700

    add findByStatus

--- example/user-service-api/src/main/java/com/alibaba/account/AccountService.java
+++ example/user-service-api/src/main/java/com/alibaba/account/AccountService.java
@@ -1,6 +1,7 @@
 package com.alibaba.account;
 
 import com.google.protobuf.Int32Value;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 /**
@@ -12,4 +13,6 @@ public interface AccountService {
     // Use protobuf for data encoding: if compile failed, please execute 'mvn compile' first to generate code from protobuf
     // 友情提示: 如果编译失败，请命令执行 mvn compile, 生成protobuf对应的Java代码。这里主要演示使用Protobuf进行对象序列化
     Mono<Account> findById(Int32Value id);
+
+    Flux<Account> findByStatus(Int32Value status);
 }
