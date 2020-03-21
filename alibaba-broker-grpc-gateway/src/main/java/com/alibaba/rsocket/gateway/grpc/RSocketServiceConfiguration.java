371e8800cec9647f4340cfd44156d906c96264a8
leijuan <jacky.chenlb@alibaba-inc.com>
2020-03-21 15:37:17 -0700

    add ReactorAccountServiceGrpc.AccountServiceImplBase

--- alibaba-broker-grpc-gateway/src/main/java/com/alibaba/rsocket/gateway/grpc/RSocketServiceConfiguration.java
+++ alibaba-broker-grpc-gateway/src/main/java/com/alibaba/rsocket/gateway/grpc/RSocketServiceConfiguration.java
@@ -1,5 +1,6 @@
 package com.alibaba.rsocket.gateway.grpc;
 
+import com.alibaba.account.ReactorAccountServiceGrpc;
 import com.alibaba.rsocket.upstream.UpstreamManager;
 import io.rsocket.RSocket;
 import org.springframework.context.annotation.Bean;
@@ -16,5 +17,13 @@ public class RSocketServiceConfiguration {
     @Bean
     public RSocket rsocketBroker(UpstreamManager upstreamManager) {
         return upstreamManager.findBroker().getLoadBalancedRSocket();
+    }
+
+    @Bean
+    public ReactorAccountServiceGrpc.AccountServiceImplBase userService(UpstreamManager upstreamManager) throws Exception {
+        return GrpcServiceRSocketImplBuilder
+                .stub(ReactorAccountServiceGrpc.AccountServiceImplBase.class, "com.alibaba.account.AccountService")
+                .upstreamManager(upstreamManager)
+                .build();
     }
 }
