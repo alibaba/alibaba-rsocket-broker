cd32ff3b17258447e138db92a7ae1f2e5302de23
leijuan <jacky.chenlb@alibaba-inc.com>
2020-03-22 14:41:14 -0700

    event reply

---
+++ alibaba-rsocket-core/src/main/java/com/alibaba/rsocket/cloudevents/EventReply.java
@@ -0,0 +1,72 @@
+package com.alibaba.rsocket.cloudevents;
+
+/**
+ * reply for the event with replyto(rsocket:///REQUEST_FNF/com.xxxx.XxxService#method) extension
+ *
+ * @author leijuan
+ */
+public class EventReply {
+    private String eventId;
+    private String eventType;
+    private Long timestamp;
+    private boolean result;
+    private String message;
+    private String appName;
+    private String uuid;
+
+    public String getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(String eventId) {
+        this.eventId = eventId;
+    }
+
+    public String getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(String eventType) {
+        this.eventType = eventType;
+    }
+
+    public Long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public boolean isResult() {
+        return result;
+    }
+
+    public void setResult(boolean result) {
+        this.result = result;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getAppName() {
+        return appName;
+    }
+
+    public void setAppName(String appName) {
+        this.appName = appName;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+}
