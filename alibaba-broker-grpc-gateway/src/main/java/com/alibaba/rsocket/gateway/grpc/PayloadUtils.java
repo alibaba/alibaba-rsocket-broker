257c746914697cdb1a8e9501fc2f118300400e59
leijuan <jacky.chenlb@alibaba-inc.com>
2020-03-21 17:25:40 -0700

    polishing

--- alibaba-broker-grpc-gateway/src/main/java/com/alibaba/rsocket/gateway/grpc/PayloadUtils.java
+++ alibaba-broker-grpc-gateway/src/main/java/com/alibaba/rsocket/gateway/grpc/PayloadUtils.java
@@ -1,23 +1,18 @@
 package com.alibaba.rsocket.gateway.grpc;
 
-import com.alibaba.rsocket.metadata.GSVRoutingMetadata;
 import com.alibaba.rsocket.metadata.MessageMimeTypeMetadata;
 import com.alibaba.rsocket.metadata.RSocketCompositeMetadata;
 import com.alibaba.rsocket.metadata.RSocketMimeType;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import com.github.benmanes.caffeine.cache.LoadingCache;
-import com.google.protobuf.GeneratedMessageV3;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.util.ReferenceCountUtil;
 import io.rsocket.Payload;
-import io.rsocket.util.ByteBufPayload;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.nio.ByteBuffer;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Payload utils
@@ -25,27 +20,19 @@ import java.util.concurrent.ConcurrentHashMap;
  * @author leijuan
  */
 public class PayloadUtils {
-    private static final Map<String, ByteBuf> compositeMetadataMap = new ConcurrentHashMap<>();
     private static final MessageMimeTypeMetadata protobufMetaEncoding = new MessageMimeTypeMetadata(RSocketMimeType.Protobuf);
+    private static ByteBuf compositeMetadataWithEncoding;
     private static final LoadingCache<Class<?>, Method> parseFromMethodStore = Caffeine.newBuilder()
             .maximumSize(Integer.MAX_VALUE)
             .build(targetClass -> targetClass.getMethod("parseFrom", ByteBuffer.class));
 
-    public static ByteBuf getCompositeMetadata(String serviceName, String methodName) {
-        String key = serviceName + "." + methodName;
-        if (!compositeMetadataMap.containsKey(key)) {
-            GSVRoutingMetadata routingMetadata = new GSVRoutingMetadata("", serviceName, methodName, "");
-            RSocketCompositeMetadata compositeMetadata = RSocketCompositeMetadata.from(routingMetadata, protobufMetaEncoding);
-            ByteBuf byteBuf = compositeMetadata.getContent();
-            compositeMetadataMap.put(key, Unpooled.copiedBuffer(byteBuf));
+    public static ByteBuf getCompositeMetaDataWithEncoding() {
+        if (compositeMetadataWithEncoding == null) {
+            ByteBuf byteBuf = RSocketCompositeMetadata.from(protobufMetaEncoding).getContent();
+            compositeMetadataWithEncoding = Unpooled.copiedBuffer(byteBuf);
             ReferenceCountUtil.release(byteBuf);
         }
-        return compositeMetadataMap.get(key);
-    }
-
-    public static Payload constructRequestPayload(GeneratedMessageV3 request, String serviceName, String method) {
-        return ByteBufPayload.create(Unpooled.wrappedBuffer(request.toByteArray()),
-                PayloadUtils.getCompositeMetadata(serviceName, method).retainedDuplicate());
+        return compositeMetadataWithEncoding;
     }
 
     public static <T> T payloadToResponseObject(Payload payload, Class<T> responseClass) throws InvocationTargetException, IllegalAccessException {
