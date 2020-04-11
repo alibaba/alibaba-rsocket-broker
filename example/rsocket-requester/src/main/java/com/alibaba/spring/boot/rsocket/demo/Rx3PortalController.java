811a570d70da03276d5e454f63af79a17bded74b
leijuan <jacky.chenlb@alibaba-inc.com>
2020-04-11 14:35:16 -0700

    add channel1

--- example/rsocket-requester/src/main/java/com/alibaba/spring/boot/rsocket/demo/Rx3PortalController.java
+++ example/rsocket-requester/src/main/java/com/alibaba/spring/boot/rsocket/demo/Rx3PortalController.java
@@ -2,7 +2,10 @@ package com.alibaba.spring.boot.rsocket.demo;
 
 import com.alibaba.user.Rx3UserService;
 import com.alibaba.user.User;
+import io.reactivex.rxjava3.core.Flowable;
+import io.reactivex.rxjava3.core.FlowableConverter;
 import io.reactivex.rxjava3.core.Maybe;
+import io.reactivex.rxjava3.core.Observable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,8 +28,13 @@ public class Rx3PortalController {
         return rx3UserService.findById(id);
     }
 
-    @RequestMapping("/")
-    public String index() {
-        return "welcome";
+    @RequestMapping("/allPeople")
+    public Flowable<User> allPeople() {
+        return rx3UserService.findAllPeople("vip");
+    }
+
+    @RequestMapping("/channel1")
+    public Observable<User> index() {
+        return rx3UserService.channel1(Observable.fromArray(1, 2));
     }
 }
