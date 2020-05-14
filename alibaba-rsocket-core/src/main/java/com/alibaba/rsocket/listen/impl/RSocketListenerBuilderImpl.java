9bc2380d203264e1aeaf3189a15216a54c217e5d
leijuan <jacky.chenlb@alibaba-inc.com>
2020-05-14 15:10:03 -0700

    polishing

--- alibaba-rsocket-core/src/main/java/com/alibaba/rsocket/listen/impl/RSocketListenerBuilderImpl.java
+++ alibaba-rsocket-core/src/main/java/com/alibaba/rsocket/listen/impl/RSocketListenerBuilderImpl.java
@@ -2,14 +2,12 @@ package com.alibaba.rsocket.listen.impl;
 
 import com.alibaba.rsocket.listen.RSocketListener;
 import io.rsocket.SocketAcceptor;
-import io.rsocket.frame.decoder.PayloadDecoder;
 import io.rsocket.plugins.DuplexConnectionInterceptor;
 import io.rsocket.plugins.RSocketInterceptor;
 import io.rsocket.plugins.SocketAcceptorInterceptor;
 
 import java.security.PrivateKey;
 import java.security.cert.Certificate;
-import java.util.function.Consumer;
 
 /**
  * RSocket listener builder implementation
@@ -17,53 +15,53 @@ import java.util.function.Consumer;
  * @author leijuan
  */
 public class RSocketListenerBuilderImpl implements RSocketListener.Builder {
-    private RSocketListenerImpl rSocketListener = new RSocketListenerImpl();
+    private RSocketListenerImpl rsocketListener = new RSocketListenerImpl();
 
     @Override
     public RSocketListener.Builder host(String host) {
-        rSocketListener.host(host);
+        rsocketListener.host(host);
         return this;
     }
 
     @Override
     public RSocketListener.Builder listen(String schema, int port) {
-        rSocketListener.listen(schema, port);
+        rsocketListener.listen(schema, port);
         return this;
     }
 
     @Override
     public RSocketListener.Builder sslContext(Certificate certificate, PrivateKey privateKey) {
-        rSocketListener.setCertificate(certificate);
-        rSocketListener.setPrivateKey(privateKey);
+        rsocketListener.setCertificate(certificate);
+        rsocketListener.setPrivateKey(privateKey);
         return this;
     }
 
     @Override
     public RSocketListener.Builder addResponderInterceptor(RSocketInterceptor interceptor) {
-        rSocketListener.addResponderInterceptor(interceptor);
+        rsocketListener.addResponderInterceptor(interceptor);
         return this;
     }
 
     @Override
     public RSocketListener.Builder addSocketAcceptorInterceptor(SocketAcceptorInterceptor interceptor) {
-        rSocketListener.addSocketAcceptorInterceptor(interceptor);
+        rsocketListener.addSocketAcceptorInterceptor(interceptor);
         return this;
     }
 
     @Override
     public RSocketListener.Builder addConnectionInterceptor(DuplexConnectionInterceptor interceptor) {
-        rSocketListener.addConnectionInterceptor(interceptor);
+        rsocketListener.addConnectionInterceptor(interceptor);
         return this;
     }
 
     @Override
     public RSocketListener.Builder acceptor(SocketAcceptor acceptor) {
-        rSocketListener.setAcceptor(acceptor);
+        rsocketListener.setAcceptor(acceptor);
         return this;
     }
 
     @Override
     public RSocketListener build() {
-        return rSocketListener;
+        return rsocketListener;
     }
 }
