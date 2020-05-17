170774e35aeba9f2796b3ce52d6de2f4a7883e7a
leijuan <jacky.chenlb@alibaba-inc.com>
2020-05-17 16:39:05 -0700

    adjust KotlinSerializerSupport to static

--- alibaba-rsocket-core/src/main/java/com/alibaba/rsocket/encoding/impl/KotlinSerializerSupport.java
+++ alibaba-rsocket-core/src/main/java/com/alibaba/rsocket/encoding/impl/KotlinSerializerSupport.java
@@ -2,6 +2,9 @@ package com.alibaba.rsocket.encoding.impl;
 
 import kotlinx.serialization.KSerializer;
 import kotlinx.serialization.Serializable;
+import kotlinx.serialization.SerializationStrategy;
+import kotlinx.serialization.cbor.Cbor;
+import kotlinx.serialization.protobuf.ProtoBuf;
 
 import java.lang.reflect.Field;
 import java.util.HashMap;
@@ -12,10 +15,11 @@ import java.util.Map;
  *
  * @author leijuan
  */
+@SuppressWarnings("unchecked")
 public class KotlinSerializerSupport {
     private static final Map<Class<?>, KSerializer<?>> ktSerializableClassStore = new HashMap<>();
 
-    protected KSerializer<?> getSerializer(Class<?> clazz) throws Exception {
+    protected static KSerializer<?> getSerializer(Class<?> clazz) throws Exception {
         KSerializer<?> kSerializer = ktSerializableClassStore.get(clazz);
         if (kSerializer == null) {
             Class<?> serializerClazz = Class.forName(clazz.getCanonicalName() + "$$serializer");
@@ -26,7 +30,24 @@ public class KotlinSerializerSupport {
         return kSerializer;
     }
 
-    protected boolean isKotlinSerializable(Class<?> clazz) {
+    protected static boolean isKotlinSerializable(Class<?> clazz) {
         return ktSerializableClassStore.containsKey(clazz) || clazz.getAnnotation(Serializable.class) != null;
     }
+
+    public static byte[] encodeAsProtobuf(Object result) throws Exception {
+        return ProtoBuf.Default.dump((SerializationStrategy<? super Object>) KotlinSerializerSupport.getSerializer(result.getClass()), result);
+    }
+
+    public static Object decodeFromProtobuf(byte[] bytes, Class<?> targetClass) throws Exception {
+        return ProtoBuf.Default.load(KotlinSerializerSupport.getSerializer(targetClass), bytes);
+    }
+
+    public static byte[] encodeAsCbor(Object result) throws Exception {
+        return Cbor.Default.dump((SerializationStrategy<? super Object>) KotlinSerializerSupport.getSerializer(result.getClass()), result);
+    }
+
+    public static Object decodeFromCbor(byte[] bytes, Class<?> targetClass) throws Exception {
+        return Cbor.Default.load(KotlinSerializerSupport.getSerializer(targetClass), bytes);
+    }
+
 }
