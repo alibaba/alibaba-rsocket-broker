002cbdf67a780e03011132ac049a37b68953b450
leijuan <jacky.chenlb@alibaba-inc.com>
2020-05-17 23:34:27 -0700

    bug fix for multi ReactiveHealthIndicator beans

--- alibaba-rsocket-spring-boot-starter/src/main/java/com/alibaba/spring/boot/rsocket/health/RSocketServiceHealthImpl.java
+++ alibaba-rsocket-spring-boot-starter/src/main/java/com/alibaba/spring/boot/rsocket/health/RSocketServiceHealthImpl.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.ReactiveHealthIndicator;
 import org.springframework.boot.actuate.health.Status;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 /**
  * RSocket service health default implementation
@@ -16,21 +19,14 @@ import reactor.core.publisher.Mono;
 @RSocketService(serviceInterface = RSocketServiceHealth.class)
 public class RSocketServiceHealthImpl implements RSocketServiceHealth {
     @Autowired
-    private ReactiveHealthIndicator healthIndicator;
+    private List<ReactiveHealthIndicator> healthIndicators;
 
     @Override
     public Mono<Integer> check(String serviceName) {
-        return healthIndicator.health()
-                .map(Health::getStatus)
-                .map(status -> {
-                    if (status == Status.UP) {
-                        return SERVING_STATUS;
-                    } else if (status == Status.DOWN) {
-                        return DOWN_STATUS;
-                    } else {
-                        return UNKNOWN_STATUS;
-                    }
-                });
-
+        return Flux.fromIterable(healthIndicators)
+                .flatMap(healthIndicator -> healthIndicator
+                        .health().map(Health::getStatus))
+                .all(status -> status == Status.UP)
+                .map(result -> result ? SERVING_STATUS : DOWN_STATUS);
     }
 }
