3baf52c97046c3be542589102049f63459f78ff6
leijuan <jacky.chenlb@alibaba-inc.com>
2020-05-29 13:26:34 -0700

    Operation parameter

---
+++ alibaba-rsocket-core/src/main/java/com/alibaba/rsocket/rpc/definition/Parameter.java
@@ -0,0 +1,54 @@
+package com.alibaba.rsocket.rpc.definition;
+
+/**
+ * Operation parameter
+ *
+ * @author leijuan
+ */
+public class Parameter {
+    private String name;
+    private String type;
+    private String inferredType;
+    private String description;
+    private boolean required;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getInferredType() {
+        return inferredType;
+    }
+
+    public void setInferredType(String inferredType) {
+        this.inferredType = inferredType;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isRequired() {
+        return required;
+    }
+
+    public void setRequired(boolean required) {
+        this.required = required;
+    }
+}
