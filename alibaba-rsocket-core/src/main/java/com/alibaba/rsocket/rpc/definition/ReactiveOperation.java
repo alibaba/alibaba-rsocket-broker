13a79a51b2e7a17d17d4444428734f06c2d7ae7b
leijuan <jacky.chenlb@alibaba-inc.com>
2020-05-29 13:26:25 -0700

    Reactive Operation

---
+++ alibaba-rsocket-core/src/main/java/com/alibaba/rsocket/rpc/definition/ReactiveOperation.java
@@ -0,0 +1,65 @@
+package com.alibaba.rsocket.rpc.definition;
+
+import java.util.List;
+
+/**
+ * Reactive Operation
+ *
+ * @author leijuan
+ */
+public class ReactiveOperation {
+    private String name;
+    private String description;
+    private boolean deprecated;
+    private String returnType;
+    private String inferredType;
+    private List<String> parameters;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDeprecated() {
+        return deprecated;
+    }
+
+    public void setDeprecated(boolean deprecated) {
+        this.deprecated = deprecated;
+    }
+
+    public String getReturnType() {
+        return returnType;
+    }
+
+    public void setReturnType(String returnType) {
+        this.returnType = returnType;
+    }
+
+    public String getInferredType() {
+        return inferredType;
+    }
+
+    public void setInferredType(String inferredType) {
+        this.inferredType = inferredType;
+    }
+
+    public List<String> getParameters() {
+        return parameters;
+    }
+
+    public void setParameters(List<String> parameters) {
+        this.parameters = parameters;
+    }
+}
