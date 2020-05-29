3a5e9ab78d112d1e72119c552c2266d8cb726eb9
leijuan <jacky.chenlb@alibaba-inc.com>
2020-05-29 16:00:20 -0700

    add queryDefinition()

--- alibaba-broker-spring-boot-starter/src/main/java/com/alibaba/spring/boot/rsocket/broker/services/ServiceQueryController.java
+++ alibaba-broker-spring-boot-starter/src/main/java/com/alibaba/spring/boot/rsocket/broker/services/ServiceQueryController.java
@@ -1,12 +1,26 @@
 package com.alibaba.spring.boot.rsocket.broker.services;
 
+import com.alibaba.rsocket.ServiceLocator;
+import com.alibaba.rsocket.metadata.GSVRoutingMetadata;
+import com.alibaba.rsocket.metadata.MessageMimeTypeMetadata;
+import com.alibaba.rsocket.metadata.RSocketCompositeMetadata;
+import com.alibaba.rsocket.metadata.RSocketMimeType;
+import com.alibaba.rsocket.observability.RsocketErrorCode;
+import com.alibaba.rsocket.rpc.ReactiveServiceDiscovery;
+import com.alibaba.spring.boot.rsocket.broker.responder.RSocketBrokerHandlerRegistry;
+import com.alibaba.spring.boot.rsocket.broker.responder.RSocketBrokerResponderHandler;
 import com.alibaba.spring.boot.rsocket.broker.route.ServiceRoutingSelector;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.rsocket.Payload;
+import io.rsocket.util.ByteBufPayload;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -19,8 +33,11 @@ import java.util.Map;
 @RestController
 @RequestMapping("/service")
 public class ServiceQueryController {
+    private static MessageMimeTypeMetadata jsonMetaEncoding = new MessageMimeTypeMetadata(RSocketMimeType.Json);
     @Autowired
     private ServiceRoutingSelector routingSelector;
+    @Autowired
+    private RSocketBrokerHandlerRegistry brokerHandlerRegistry;
 
     @GetMapping("/{serviceName}")
     public Flux<Map<String, Object>> query(@PathVariable(name = "serviceName") String serviceName) {
@@ -37,6 +54,23 @@ public class ServiceQueryController {
                     }
                     return serviceInfo;
                 });
+    }
+
+    @GetMapping(value = "/definition/{serviceName}")
+    public Mono<String> queryDefinition(@PathVariable(name = "serviceName") String serviceName) {
+        Integer handler = routingSelector.findHandler(new ServiceLocator("", serviceName, "").getId());
+        if (handler != null) {
+            RSocketBrokerResponderHandler brokerResponderHandler = brokerHandlerRegistry.findById(handler);
+            if (brokerResponderHandler != null) {
+                GSVRoutingMetadata routingMetadata = new GSVRoutingMetadata("", ReactiveServiceDiscovery.class.getCanonicalName() + ".findServiceByFullName", "");
+                RSocketCompositeMetadata compositeMetadata = RSocketCompositeMetadata.from(routingMetadata, jsonMetaEncoding);
+                ByteBuf bodyBuf = Unpooled.wrappedBuffer(("[\"" + serviceName + "\"]").getBytes());
+                return brokerResponderHandler.getPeerRsocket()
+                        .requestResponse(ByteBufPayload.create(bodyBuf, compositeMetadata.getContent()))
+                        .map(Payload::getDataUtf8);
+            }
+        }
+        return Mono.error(new Exception(RsocketErrorCode.message("RST-900404", serviceName)));
     }
 
 }
