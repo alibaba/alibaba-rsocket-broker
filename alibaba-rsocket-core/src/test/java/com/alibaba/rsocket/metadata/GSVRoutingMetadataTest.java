3e857c07efb6117aaa61152db18328a6e33c7c94
leijuan <jacky.chenlb@alibaba-inc.com>
2020-07-16 12:06:42 -0700

    add testSticky

--- alibaba-rsocket-core/src/test/java/com/alibaba/rsocket/metadata/GSVRoutingMetadataTest.java
+++ alibaba-rsocket-core/src/test/java/com/alibaba/rsocket/metadata/GSVRoutingMetadataTest.java
@@ -1,6 +1,12 @@
 package com.alibaba.rsocket.metadata;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import reactor.util.function.Tuple2;
+import reactor.util.function.Tuples;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -33,5 +39,24 @@ public class GSVRoutingMetadataTest {
         String gsv = routing.gsv();
         System.out.println(gsv);
         assertThat(gsv).doesNotContain(":");
+    }
+
+    @Test
+    public void testSticky() {
+        List<Tuple2<Boolean, String>> tuples = Arrays.asList(Tuples.of(true, ""), Tuples.of(false, "id:xxxx"));
+        for (Tuple2<Boolean, String> tuple : tuples) {
+            GSVRoutingMetadata routing = new GSVRoutingMetadata();
+            routing.setService("com.alibaba.UserService");
+            routing.setMethod("findById");
+            routing.setSticky(tuple.getT1());
+            routing.setEndpoint(tuple.getT2());
+            String routingKey = routing.assembleRoutingKey();
+            GSVRoutingMetadata routing2 = GSVRoutingMetadata.from(routingKey);
+            Assertions.assertEquals(routing.isSticky(), routing2.isSticky());
+            Assertions.assertEquals(routing.getService(), routing2.getService());
+            Assertions.assertEquals(routing.getMethod(), routing2.getMethod());
+            Assertions.assertEquals(routing.getEndpoint(), routing2.getEndpoint());
+        }
+
     }
 }
