605a2dc2a64da91ab0e8ef856dbd1185e5a6b4d3
leijuan <jacky.chenlb@alibaba-inc.com>
2020-07-24 17:01:29 -0700

    add testEncodeInteger

--- example/rsocket-requester/src/test/java/com/alibaba/hessian/HessianTest.java
+++ example/rsocket-requester/src/test/java/com/alibaba/hessian/HessianTest.java
@@ -2,6 +2,7 @@ package com.alibaba.hessian;
 
 import com.caucho.hessian.io.HessianSerializerInput;
 import com.caucho.hessian.io.HessianSerializerOutput;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
@@ -24,6 +25,16 @@ public class HessianTest {
         output.flush();
         content = bos.toByteArray();
         System.out.println(content.length);
+    }
+
+    @Test
+    public void testEncodeInteger() throws Exception {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        HessianSerializerOutput output = new HessianSerializerOutput(bos);
+        output.writeObject(1);
+        output.flush();
+        content = bos.toByteArray();
+        Assertions.assertEquals(content[0], -111);
     }
 
     @Test
