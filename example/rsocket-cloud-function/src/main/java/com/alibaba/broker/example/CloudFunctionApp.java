a49292abcbe9c055316bff195711c21923994a8e
leijuan <jacky.chenlb@alibaba-inc.com>
2020-10-07 14:16:41 -0700

    CloudFunctionApp

---
+++ example/rsocket-cloud-function/src/main/java/com/alibaba/broker/example/CloudFunctionApp.java
@@ -0,0 +1,13 @@
+package com.alibaba.broker.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudFunctionApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudFunctionApp.class, args);
+    }
+
+}
