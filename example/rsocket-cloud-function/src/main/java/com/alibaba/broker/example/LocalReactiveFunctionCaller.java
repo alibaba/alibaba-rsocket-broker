5e5a99d0dd7cec44b50bc69130ae09e24117f4c8
leijuan <jacky.chenlb@alibaba-inc.com>
2020-10-07 14:28:45 -0700

    local Reactive Cloud function caller

---
+++ example/rsocket-cloud-function/src/main/java/com/alibaba/broker/example/LocalReactiveFunctionCaller.java
@@ -0,0 +1,93 @@
+package com.alibaba.broker.example;
+
+import com.alibaba.rsocket.rpc.LocalReactiveServiceCaller;
+import com.alibaba.rsocket.rpc.ReactiveMethodHandler;
+import com.alibaba.rsocket.utils.MurmurHash3;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.springframework.cloud.function.context.FunctionRegistry;
+import org.springframework.cloud.function.context.catalog.SimpleFunctionRegistry;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * local Reactive Cloud function caller
+ *
+ * @author leijuan
+ */
+public class LocalReactiveFunctionCaller implements LocalReactiveServiceCaller {
+    private Set<String> functionServices;
+    private Set<String> functionNames;
+    private Set<Integer> rsocketHashCodeServices = new HashSet<>();
+    private Map<String, ReactiveMethodHandler> methodInvokeEntrances = new HashMap<>();
+    private Map<Integer, ReactiveMethodHandler> methodHashCodeInvokeEntrances = new HashMap<>();
+
+    public LocalReactiveFunctionCaller(FunctionRegistry functionRegistry) {
+        this.functionNames = functionRegistry.getNames(null).stream()
+                .filter(functionName -> functionName.contains("."))
+                .collect(Collectors.toSet());
+        this.functionServices = this.functionNames.stream()
+                .map(functionName -> functionName.substring(0, functionName.lastIndexOf(".")))
+                .collect(Collectors.toSet());
+        for (String functionService : this.functionServices) {
+            rsocketHashCodeServices.add(MurmurHash3.hash32(functionService));
+        }
+        for (String functionName : this.functionNames) {
+            SimpleFunctionRegistry.FunctionInvocationWrapper function = functionRegistry.lookup(functionName);
+            try {
+                Method method = function.getClass().getMethod("apply", Object.class);
+                String serviceName = functionName.substring(0, functionName.lastIndexOf("."));
+                ReactiveMethodHandler reactiveMethodHandler = new ReactiveMethodHandler(serviceName, method, function);
+                methodInvokeEntrances.put(functionName, reactiveMethodHandler);
+                methodHashCodeInvokeEntrances.put(MurmurHash3.hash32(functionName), reactiveMethodHandler);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public boolean contains(String serviceName, String rpc) {
+        return functionNames.contains(serviceName + "." + rpc);
+    }
+
+    @Override
+    public boolean contains(String serviceName) {
+        return functionServices.contains(serviceName);
+    }
+
+    @Override
+    public boolean contains(Integer serviceId) {
+        return rsocketHashCodeServices.contains(serviceId);
+    }
+
+    @Override
+    public Set<String> findAllServices() {
+        return functionServices;
+    }
+
+    @Override
+    public void addProvider(@NotNull String group, String serviceName, @NotNull String version, Class<?> serviceInterface, Object handler) {
+        throw new RuntimeException("Custom service registration not support!");
+    }
+
+    @Override
+    public @Nullable ReactiveMethodHandler getInvokeMethod(String serviceName, String method) {
+        return methodInvokeEntrances.get(serviceName + "." + method);
+    }
+
+    @Override
+    public @Nullable ReactiveMethodHandler getInvokeMethod(Integer handlerId) {
+        return methodHashCodeInvokeEntrances.get(handlerId);
+    }
+
+    @Override
+    public boolean containsHandler(Integer handlerId) {
+        return methodHashCodeInvokeEntrances.containsKey(handlerId);
+    }
+}
