ce431f09b87bae66e5fd4824022d602be837c4b5
leijuan <jacky.chenlb@alibaba-inc.com>
2020-10-07 13:57:23 -0700

    add requesterSupport

--- alibaba-rsocket-spring-boot-starter/src/main/java/com/alibaba/spring/boot/rsocket/upstream/RSocketRequesterSupportBuilderImpl.java
+++ alibaba-rsocket-spring-boot-starter/src/main/java/com/alibaba/spring/boot/rsocket/upstream/RSocketRequesterSupportBuilderImpl.java
@@ -6,6 +6,8 @@ import com.alibaba.spring.boot.rsocket.RSocketRequesterSupportImpl;
 import io.rsocket.SocketAcceptor;
 import io.rsocket.plugins.RSocketInterceptor;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 /**
@@ -14,7 +16,9 @@ import java.util.Properties;
  * @author leijuan
  */
 public class RSocketRequesterSupportBuilderImpl implements RSocketRequesterSupportBuilder {
-    private RSocketRequesterSupportImpl requesterSupport;
+    private RSocketRequesterSupport requesterSupport;
+    private List<RSocketInterceptor> requesterInterceptors = new ArrayList<>();
+    private List<RSocketInterceptor> responderInterceptors = new ArrayList<>();
 
     public RSocketRequesterSupportBuilderImpl(RSocketProperties properties,
                                               Properties env,
@@ -22,18 +26,31 @@ public class RSocketRequesterSupportBuilderImpl implements RSocketRequesterSuppo
         this.requesterSupport = new RSocketRequesterSupportImpl(properties, env, socketAcceptor);
     }
 
+    public RSocketRequesterSupportBuilder requesterSupport(RSocketRequesterSupport requesterSupport) {
+        this.requesterSupport = requesterSupport;
+        return this;
+    }
+
     @Override
     public RSocketRequesterSupportBuilder addResponderInterceptor(RSocketInterceptor interceptor) {
+        this.responderInterceptors.add(interceptor);
         return this;
     }
 
     @Override
     public RSocketRequesterSupportBuilder addRequesterInterceptor(RSocketInterceptor interceptor) {
+        this.requesterInterceptors.add(interceptor);
         return this;
     }
 
     @Override
     public RSocketRequesterSupport build() {
-        return requesterSupport;
+        if (!this.responderInterceptors.isEmpty()) {
+            this.requesterSupport.responderInterceptors().addAll(responderInterceptors);
+        }
+        if (!this.requesterInterceptors.isEmpty()) {
+            this.requesterSupport.requestInterceptors().addAll(requesterInterceptors);
+        }
+        return this.requesterSupport;
     }
 }
