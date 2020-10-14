ea8061777c154a767ef29fa81f6d0a157f8831db
leijuan <jacky.chenlb@alibaba-inc.com>
2020-10-13 17:58:58 -0700

    optimize isKotlinSerializable()

--- alibaba-rsocket-core/src/main/java/com/alibaba/rsocket/encoding/impl/KotlinSerializerSupport.java
+++ alibaba-rsocket-core/src/main/java/com/alibaba/rsocket/encoding/impl/KotlinSerializerSupport.java
@@ -4,11 +4,15 @@ import kotlinx.serialization.KSerializer;
 import kotlinx.serialization.Serializable;
 import kotlinx.serialization.SerializationStrategy;
 import kotlinx.serialization.cbor.Cbor;
+import kotlinx.serialization.json.Json;
 import kotlinx.serialization.protobuf.ProtoBuf;
 
 import java.lang.reflect.Field;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Kotlin Serializer support
@@ -18,6 +22,7 @@ import java.util.Map;
 @SuppressWarnings("unchecked")
 public class KotlinSerializerSupport {
     private static final Map<Class<?>, KSerializer<?>> ktSerializableClassStore = new HashMap<>();
+    private static final Set<Class<?>> normalClassSet = new HashSet<>();
 
     protected static KSerializer<?> getSerializer(Class<?> clazz) throws Exception {
         KSerializer<?> kSerializer = ktSerializableClassStore.get(clazz);
@@ -31,7 +36,22 @@ public class KotlinSerializerSupport {
     }
 
     protected static boolean isKotlinSerializable(Class<?> clazz) {
-        return ktSerializableClassStore.containsKey(clazz) || clazz.getAnnotation(Serializable.class) != null;
+        if (ktSerializableClassStore.containsKey(clazz)) {
+            return true;
+        } else if (normalClassSet.contains(clazz)) {
+            return false;
+        } else {
+            if (clazz.getAnnotation(Serializable.class) != null) {
+                try {
+                    getSerializer(clazz);
+                } catch (Exception ignore) {
+                }
+                return true;
+            } else {
+                normalClassSet.add(clazz);
+                return false;
+            }
+        }
     }
 
     public static byte[] encodeAsProtobuf(Object result) throws Exception {
@@ -48,6 +68,15 @@ public class KotlinSerializerSupport {
 
     public static Object decodeFromCbor(byte[] bytes, Class<?> targetClass) throws Exception {
         return Cbor.Default.decodeFromByteArray(KotlinSerializerSupport.getSerializer(targetClass), bytes);
+    }
+
+    public static byte[] encodeAsJson(Object result) throws Exception {
+        return Json.Default.encodeToString((SerializationStrategy<? super Object>) KotlinSerializerSupport.getSerializer(result.getClass()), result)
+                .getBytes(StandardCharsets.UTF_8);
+    }
+
+    public static Object decodeFromJson(byte[] bytes, Class<?> targetClass) throws Exception {
+        return Json.Default.decodeFromString(KotlinSerializerSupport.getSerializer(targetClass), new String(bytes, StandardCharsets.UTF_8));
     }
 
 }
