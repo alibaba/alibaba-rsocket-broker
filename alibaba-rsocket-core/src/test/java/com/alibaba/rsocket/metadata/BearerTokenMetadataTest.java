03f362dfa5dd904b94d8d115b2b79979196a7cb5
leijuan <jacky.chenlb@alibaba-inc.com>
2020-10-28 23:57:19 -0700

    introduce TaggingMetadataCodec

--- alibaba-rsocket-core/src/test/java/com/alibaba/rsocket/metadata/BearerTokenMetadataTest.java
+++ alibaba-rsocket-core/src/test/java/com/alibaba/rsocket/metadata/BearerTokenMetadataTest.java
@@ -1,9 +1,8 @@
 package com.alibaba.rsocket.metadata;
 
 import io.netty.buffer.ByteBuf;
-import io.netty.buffer.ByteBufAllocator;
 import io.netty.buffer.PooledByteBufAllocator;
-import io.rsocket.metadata.security.AuthMetadataFlyweight;
+import io.rsocket.metadata.AuthMetadataCodec;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -25,16 +24,16 @@ public class BearerTokenMetadataTest {
         Assertions.assertEquals(token.length() + 1, tokenMetadata.getContent().readableBytes());
         BearerTokenMetadata tokenMetadata1 = BearerTokenMetadata.from(tokenMetadata.getContent());
         Assertions.assertEquals(String.valueOf(tokenMetadata1.getBearerToken()), token);
-        ByteBuf byteBuf = AuthMetadataFlyweight.encodeBearerMetadata(PooledByteBufAllocator.DEFAULT, token.toCharArray());
+        ByteBuf byteBuf = AuthMetadataCodec.encodeBearerMetadata(PooledByteBufAllocator.DEFAULT, token.toCharArray());
         assertThat(toArrayString(tokenMetadata.getContent())).isEqualTo(toArrayString(byteBuf));
     }
 
     @Test
     public void testDecode() {
         String token = "123456";
-        ByteBuf byteBuf = AuthMetadataFlyweight.encodeBearerMetadata(PooledByteBufAllocator.DEFAULT, token.toCharArray()).duplicate();
-        AuthMetadataFlyweight.decodeWellKnownAuthType(byteBuf);
-        String token2 = new String(AuthMetadataFlyweight.decodeBearerTokenAsCharArray(byteBuf));
+        ByteBuf byteBuf = AuthMetadataCodec.encodeBearerMetadata(PooledByteBufAllocator.DEFAULT, token.toCharArray()).duplicate();
+        AuthMetadataCodec.readWellKnownAuthType(byteBuf);
+        String token2 = new String(AuthMetadataCodec.readBearerTokenAsCharArray(byteBuf));
         assertThat(token).isEqualTo(token2);
     }
 
