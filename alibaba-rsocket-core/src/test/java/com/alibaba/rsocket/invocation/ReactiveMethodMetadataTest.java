499fc2f8b19babd4f58d56ea2667b9555503fdac
leijuan <jacky.chenlb@alibaba-inc.com>
2021-03-09 00:16:18 -0800

    Add testCloudEventReturn

--- alibaba-rsocket-core/src/test/java/com/alibaba/rsocket/invocation/ReactiveMethodMetadataTest.java
+++ alibaba-rsocket-core/src/test/java/com/alibaba/rsocket/invocation/ReactiveMethodMetadataTest.java
@@ -1,6 +1,7 @@
 package com.alibaba.rsocket.invocation;
 
 import com.alibaba.rsocket.metadata.RSocketMimeType;
+import io.cloudevents.CloudEvent;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Mono;
@@ -24,7 +25,20 @@ public class ReactiveMethodMetadataTest {
         Assertions.assertThat(methodMetadata.getInferredClassForReturn()).isEqualTo(ByteBuffer.class);
     }
 
+    @Test
+    public void testCloudEventReturn() throws Exception {
+        Method rawContentMethod = this.getClass().getMethod("fireEvent", CloudEvent.class);
+        ReactiveMethodMetadata methodMetadata = new ReactiveMethodMetadata(null, "com.alibaba.user.UserService", "",
+                rawContentMethod, RSocketMimeType.Hessian, new RSocketMimeType[]{}, null, false, URI.create("tcp://127.0.0.1:0?appName=demo"));
+        Assertions.assertThat(methodMetadata.getAcceptEncodingTypes()).contains(RSocketMimeType.CloudEventsJson);
+        Assertions.assertThat(methodMetadata.getParamEncoding()).isEqualTo(RSocketMimeType.CloudEventsJson);
+    }
+
     public Mono<ByteBuffer> findById(Integer id) {
+        return Mono.empty();
+    }
+
+    public Mono<CloudEvent> fireEvent(CloudEvent event) {
         return Mono.empty();
     }
 }
