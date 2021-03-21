65a6f4f57d533cf735525a3e107b6865f41b9906
leijuan <jacky.chenlb@alibaba-inc.com>
2021-03-20 19:33:26 -0700

    Add Vaadin annotations

--- alibaba-broker-server/src/main/java/com/alibaba/rsocket/broker/AlibabaRSocketBrokerServer.java
+++ alibaba-broker-server/src/main/java/com/alibaba/rsocket/broker/AlibabaRSocketBrokerServer.java
@@ -1,6 +1,12 @@
 package com.alibaba.rsocket.broker;
 
 import com.alibaba.rsocket.encoding.RSocketEncodingFacade;
+import com.vaadin.flow.component.dependency.StyleSheet;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.component.page.Push;
+import com.vaadin.flow.component.page.Viewport;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
 import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
@@ -15,7 +21,11 @@ import java.time.LocalDateTime;
  * @author leijuan
  */
 @SpringBootApplication
-public class AlibabaRSocketBrokerServer {
+@StyleSheet("styles/styles.css")
+@Theme(themeClass = Lumo.class)
+@Viewport("width=device-width, minimum-scale=1, initial-scale=1, user-scalable=yes, viewport-fit=cover")
+@Push
+public class AlibabaRSocketBrokerServer implements AppShellConfigurator {
     public static final LocalDateTime STARTED_AT = LocalDateTime.now();
 
     public static void main(String[] args) {
