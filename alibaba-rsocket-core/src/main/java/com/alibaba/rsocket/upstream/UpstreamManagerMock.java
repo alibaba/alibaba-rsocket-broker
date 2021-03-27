7af3a823ae367dadd2c5451cdf88c4ec5be907e4
leijuan <jacky.chenlb@alibaba-inc.com>
2021-03-27 01:41:55 -0700

    Upstream Manager mock for unit test

---
+++ alibaba-rsocket-core/src/main/java/com/alibaba/rsocket/upstream/UpstreamManagerMock.java
@@ -0,0 +1,117 @@
+package com.alibaba.rsocket.upstream;
+
+import com.alibaba.rsocket.RSocketRequesterSupport;
+import com.alibaba.rsocket.client.SimpleRSocketRequesterSupport;
+import com.alibaba.rsocket.cloudevents.CloudEventImpl;
+import com.alibaba.rsocket.rpc.LocalReactiveServiceCallerImpl;
+import io.rsocket.Payload;
+import io.rsocket.RSocket;
+import org.jetbrains.annotations.NotNull;
+import org.reactivestreams.Publisher;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.extra.processor.TopicProcessor;
+
+import java.util.*;
+
+/**
+ * Upstream Manager mock for unit test
+ *
+ * @author leijuan
+ */
+public class UpstreamManagerMock implements UpstreamManager {
+    private final Map<String, UpstreamCluster> clusters = new HashMap<>();
+    private UpstreamCluster brokerCluster;
+    private RSocketRequesterSupport rsocketRequesterSupport;
+    private RSocket mockRSocket;
+
+    public UpstreamManagerMock() {
+        this.rsocketRequesterSupport = new SimpleRSocketRequesterSupport("MockApp", "".toCharArray(),
+                Collections.EMPTY_LIST,
+                new LocalReactiveServiceCallerImpl(),
+                TopicProcessor.<CloudEventImpl>builder().name("cloud-events-processor").build());
+        mockRSocket = new RSocket() {
+            @Override
+            @NotNull
+            public Mono<Void> fireAndForget(@NotNull Payload payload) {
+                return Mono.empty();
+            }
+
+            @Override
+            @NotNull
+            public Mono<Payload> requestResponse(@NotNull Payload payload) {
+                return Mono.empty();
+            }
+
+            @Override
+            @NotNull
+            public Flux<Payload> requestStream(@NotNull Payload payload) {
+                return Flux.empty();
+            }
+
+            @Override
+            @NotNull
+            public Flux<Payload> requestChannel(@NotNull Publisher<Payload> payloads) {
+                return Flux.empty();
+            }
+
+            @Override
+            @NotNull
+            public Mono<Void> metadataPush(@NotNull Payload payload) {
+                return Mono.empty();
+            }
+        };
+        this.add(new UpstreamCluster("", "*", ""));
+    }
+
+    @Override
+    public void add(UpstreamCluster cluster) {
+        clusters.put(cluster.getServiceId(), cluster);
+        if (cluster.isBroker()) {
+            this.brokerCluster = cluster;
+        }
+    }
+
+    @Override
+    public Collection<UpstreamCluster> findAllClusters() {
+        return clusters.values();
+    }
+
+    @Override
+    public UpstreamCluster findClusterByServiceId(String serviceId) {
+        return this.brokerCluster;
+    }
+
+    @Override
+    public UpstreamCluster findBroker() {
+        return this.brokerCluster;
+    }
+
+    @Override
+    public RSocket getRSocket(String serviceId) {
+        return this.mockRSocket;
+    }
+
+    @Override
+    public RSocketRequesterSupport requesterSupport() {
+        return this.rsocketRequesterSupport;
+    }
+
+    @Override
+    public void refresh(String serviceId, List<String> uris) {
+
+    }
+
+    @Override
+    public void init() throws Exception {
+        for (UpstreamCluster cluster : clusters.values()) {
+            cluster.setRsocketAware(rsocketRequesterSupport);
+            cluster.init();
+        }
+    }
+
+    @Override
+    public void close() {
+
+    }
+}
