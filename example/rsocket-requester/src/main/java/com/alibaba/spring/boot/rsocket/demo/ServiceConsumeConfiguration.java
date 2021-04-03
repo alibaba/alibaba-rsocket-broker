d079cb6a219c2ec044d5523007628e26e2df7d27
leijuan <jacky.chenlb@alibaba-inc.com>
2021-04-03 15:44:05 -0700

    Remove some beans that replaced by user-service-spring-booot-starter

--- example/rsocket-requester/src/main/java/com/alibaba/spring/boot/rsocket/demo/ServiceConsumeConfiguration.java
+++ example/rsocket-requester/src/main/java/com/alibaba/spring/boot/rsocket/demo/ServiceConsumeConfiguration.java
@@ -23,15 +23,6 @@ import org.springframework.util.MimeType;
  */
 @Configuration
 public class ServiceConsumeConfiguration {
-    @Bean
-    public UserService userService(UpstreamManager upstreamManager) {
-        return RSocketRemoteServiceBuilder
-                .client(UserService.class)
-                //.sticky(true)
-                .upstreamManager(upstreamManager)
-                //.endpoint("ip:192.168.1.2") //for testing
-                .build();
-    }
 
     @Bean
     public UserServiceExtra userServiceExtra(UpstreamManager upstreamManager) {
@@ -40,33 +31,6 @@ public class ServiceConsumeConfiguration {
                 .service(UserService.class.getCanonicalName())
                 .upstreamManager(upstreamManager)
                 .acceptEncodingType(RSocketMimeType.Json)
-                .build();
-    }
-
-    @Bean
-    public RxUserService rxUserService(UpstreamManager upstreamManager) {
-        return RSocketRemoteServiceBuilder
-                .client(RxUserService.class)
-                .encodingType(RSocketMimeType.CBOR)
-                .upstreamManager(upstreamManager)
-                .build();
-    }
-
-    @Bean
-    public Rx3UserService rx3UserService(UpstreamManager upstreamManager) {
-        return RSocketRemoteServiceBuilder
-                .client(Rx3UserService.class)
-                .upstreamManager(upstreamManager)
-                .build();
-    }
-
-
-    @Bean
-    public AccountService accountService(UpstreamManager upstreamManager) {
-        return RSocketRemoteServiceBuilder
-                .client(AccountService.class)
-                .encodingType(RSocketMimeType.Protobuf)
-                .upstreamManager(upstreamManager)
                 .build();
     }
 
