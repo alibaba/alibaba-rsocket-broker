a936259b0d3c404113e67914987fe5cbdf5636f0
leijuan <jacky.chenlb@alibaba-inc.com>
2021-04-03 15:43:13 -0700

    UserServicesAutoConfiguration

---
+++ example/user-service-spring-boot-starter/src/main/java/com/alibaba/user/springboot/UserServicesAutoConfiguration.java
@@ -0,0 +1,56 @@
+package com.alibaba.user.springboot;
+
+import com.alibaba.rsocket.invocation.RSocketRemoteServiceBuilder;
+import com.alibaba.rsocket.metadata.RSocketMimeType;
+import com.alibaba.rsocket.upstream.UpstreamManager;
+import com.alibaba.user.AccountService;
+import com.alibaba.user.Rx3UserService;
+import com.alibaba.user.RxUserService;
+import com.alibaba.user.UserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * User services auto configuration for Spring Boot
+ *
+ * @author leijuan
+ */
+@Configuration(proxyBeanMethods = false)
+public class UserServicesAutoConfiguration {
+
+    @Bean
+    public UserService userService(UpstreamManager upstreamManager) {
+        return RSocketRemoteServiceBuilder
+                .client(UserService.class)
+                .upstreamManager(upstreamManager)
+                .build();
+    }
+
+
+    @Bean
+    public RxUserService rxUserService(UpstreamManager upstreamManager) {
+        return RSocketRemoteServiceBuilder
+                .client(RxUserService.class)
+                .encodingType(RSocketMimeType.CBOR)
+                .upstreamManager(upstreamManager)
+                .build();
+    }
+
+    @Bean
+    public Rx3UserService rx3UserService(UpstreamManager upstreamManager) {
+        return RSocketRemoteServiceBuilder
+                .client(Rx3UserService.class)
+                .upstreamManager(upstreamManager)
+                .build();
+    }
+
+    @Bean
+    public AccountService accountService(UpstreamManager upstreamManager) {
+        return RSocketRemoteServiceBuilder
+                .client(AccountService.class)
+                .encodingType(RSocketMimeType.Protobuf)
+                .upstreamManager(upstreamManager)
+                .build();
+    }
+
+}
