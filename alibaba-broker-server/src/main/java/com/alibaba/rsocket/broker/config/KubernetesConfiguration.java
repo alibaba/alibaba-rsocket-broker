b630c5004eb93e11fae1e86207cbb7fc9491c49e
leijuan <jacky.chenlb@alibaba-inc.com>
2021-04-15 15:19:56 -0700

    KubernetesConfiguration

---
+++ alibaba-broker-server/src/main/java/com/alibaba/rsocket/broker/config/KubernetesConfiguration.java
@@ -0,0 +1,12 @@
+package com.alibaba.rsocket.broker.config;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@EnableDiscoveryClient
+@Profile("k8s")
+public class KubernetesConfiguration {
+
+}
