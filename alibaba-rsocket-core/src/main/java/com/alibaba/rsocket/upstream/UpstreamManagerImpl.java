8ae469dea3e5d2ef7e33f48f6fe2c167ddafc76f
leijuan <jacky.chenlb@alibaba-inc.com>
2021-04-23 13:15:47 -0700

    Add monitorClusters()

--- alibaba-rsocket-core/src/main/java/com/alibaba/rsocket/upstream/UpstreamManagerImpl.java
+++ alibaba-rsocket-core/src/main/java/com/alibaba/rsocket/upstream/UpstreamManagerImpl.java
@@ -1,15 +1,23 @@
 package com.alibaba.rsocket.upstream;
 
 import com.alibaba.rsocket.RSocketRequesterSupport;
+import com.alibaba.rsocket.discovery.DiscoveryService;
+import com.alibaba.rsocket.discovery.RSocketServiceInstance;
+import com.alibaba.rsocket.invocation.RSocketRequesterRpcProxy;
+import com.alibaba.rsocket.metadata.RSocketMimeType;
 import com.alibaba.rsocket.observability.RsocketErrorCode;
 import io.rsocket.RSocket;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
 
+import java.lang.reflect.Proxy;
+import java.time.Duration;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * upstream manager implementation
@@ -20,7 +28,9 @@ public class UpstreamManagerImpl implements UpstreamManager {
     private static final Logger log = LoggerFactory.getLogger(UpstreamManagerImpl.class);
     private final Map<String, UpstreamCluster> clusters = new HashMap<>();
     private UpstreamCluster brokerCluster;
+    private DiscoveryService brokerDiscoveryService;
     private RSocketRequesterSupport rsocketRequesterSupport;
+    private int status = 0;
 
     public UpstreamManagerImpl(RSocketRequesterSupport rsocketRequesterSupport) {
         this.rsocketRequesterSupport = rsocketRequesterSupport;
@@ -50,6 +60,24 @@ public class UpstreamManagerImpl implements UpstreamManager {
     }
 
     @Override
+    public DiscoveryService findBrokerDiscoveryService() {
+        if (brokerCluster != null && brokerDiscoveryService == null) {
+            RSocketRequesterRpcProxy proxy = new RSocketRequesterRpcProxy(brokerCluster, "",
+                    DiscoveryService.class,
+                    DiscoveryService.class.getCanonicalName(),
+                    "", RSocketMimeType.Hessian, RSocketMimeType.Hessian,
+                    Duration.ofMillis(3000), null, false,
+                    rsocketRequesterSupport.originUri(), true
+            );
+            this.brokerDiscoveryService = (DiscoveryService) Proxy.newProxyInstance(
+                    DiscoveryService.class.getClassLoader(),
+                    new Class[]{DiscoveryService.class},
+                    proxy);
+        }
+        return brokerDiscoveryService;
+    }
+
+    @Override
     public RSocket getRSocket(String serviceId) {
         return clusters.get(serviceId).getLoadBalancedRSocket();
     }
@@ -72,6 +100,8 @@ public class UpstreamManagerImpl implements UpstreamManager {
                 cluster.init();
             }
         }
+        monitorClusters();
+        status = 1;
     }
 
     @Override
@@ -83,6 +113,16 @@ public class UpstreamManagerImpl implements UpstreamManager {
             } catch (Exception e) {
                 log.error(RsocketErrorCode.message("RST-400001"), e);
             }
+        }
+    }
+
+    public void monitorClusters() {
+        if (status == 0 && brokerCluster != null) {
+            //interval sync to broker to get last broker list in case of UpstreamClusterChangedEvent lost
+            Flux.interval(Duration.ofSeconds(60))
+                    .flatMap(timestamp -> findBrokerDiscoveryService().getInstances("*").collectList())
+                    .map(serviceInstances -> serviceInstances.stream().map(RSocketServiceInstance::getUri).collect(Collectors.toList()))
+                    .subscribe(uris -> brokerCluster.setUris(uris));
         }
     }
 
