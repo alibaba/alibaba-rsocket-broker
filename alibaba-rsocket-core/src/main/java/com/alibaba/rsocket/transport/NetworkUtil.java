12e0e7c4229bd7217b4e147716f72dc6982d3db9
leijuan <jacky.chenlb@alibaba-inc.com>
2021-04-28 16:46:20 -0700

    Add isInternalIp

--- alibaba-rsocket-core/src/main/java/com/alibaba/rsocket/transport/NetworkUtil.java
+++ alibaba-rsocket-core/src/main/java/com/alibaba/rsocket/transport/NetworkUtil.java
@@ -42,4 +42,50 @@ public class NetworkUtil {
         }
         return ip;
     }
+
+    public static boolean isInternalIp(String ipOrHost) {
+        try {
+            return isInternalIp(InetAddress.getByName(ipOrHost).getAddress());
+        } catch (Exception e) {
+            System.out.println("Failed to get ip:" + e.getMessage());
+            return true;
+        }
+    }
+
+    public static boolean isInternalIp(byte[] addr) {
+        final byte b0 = addr[0];
+        final byte b1 = addr[1];
+        final byte b2 = addr[2];
+        final byte b3 = addr[3];
+        //10.x.x.x/8
+        final byte SECTION_1 = 0x0A;
+        //172.16.x.x/12
+        final byte SECTION_2 = (byte) 0xAC;
+        final byte SECTION_3 = (byte) 0x10;
+        final byte SECTION_4 = (byte) 0x1F;
+        //192.168.x.x/16
+        final byte SECTION_5 = (byte) 0xC0;
+        final byte SECTION_6 = (byte) 0xA8;
+        //127.0.0.1
+        final byte SECTION_7 = (byte) 127;
+        switch (b0) {
+            case SECTION_1:
+                return true;
+            case SECTION_2:
+                if (b1 >= SECTION_3 && b1 <= SECTION_4) {
+                    return true;
+                }
+            case SECTION_5:
+                if (b1 == SECTION_6) {
+                    return true;
+                }
+            case SECTION_7:
+                if (b1 == 0 && b2 == 0 && b3 == 1) {
+                    return true;
+                }
+            default:
+                return false;
+        }
+    }
 }
+ 
