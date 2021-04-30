3077ddec21b42c8c31632a68178a9b19b7cc9eb8
leijuan <jacky.chenlb@alibaba-inc.com>
2021-04-29 21:38:50 -0700

    UserService2Controller

---
+++ example/rsocket-requester/src/main/java/com/alibaba/spring/boot/rsocket/demo/UserService2Controller.java
@@ -0,0 +1,22 @@
+package com.alibaba.spring.boot.rsocket.demo;
+
+import com.alibaba.user.User;
+import com.alibaba.user.UserService2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+
+@RestController
+public class UserService2Controller {
+    @Autowired
+    private UserService2 userService2;
+
+    @GetMapping("/user2/{id}")
+    public Mono<User> user(@PathVariable Integer id) {
+        return userService2.findById(id);
+    }
+
+}
