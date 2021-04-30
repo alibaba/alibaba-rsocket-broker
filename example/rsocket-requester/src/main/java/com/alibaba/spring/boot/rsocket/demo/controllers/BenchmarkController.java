1134212fad90d7862d6acfa440deb09e155cba5b
leijuan <jacky.chenlb@alibaba-inc.com>
2021-04-30 00:08:46 -0700

    Polishing

--- example/rsocket-requester/src/main/java/com/alibaba/spring/boot/rsocket/demo/controllers/BenchmarkController.java
+++ example/rsocket-requester/src/main/java/com/alibaba/spring/boot/rsocket/demo/controllers/BenchmarkController.java
@@ -3,8 +3,6 @@ package com.alibaba.spring.boot.rsocket.demo.controllers;
 import com.alibaba.user.UserService;
 import com.alibaba.user.UserService2;
 import org.HdrHistogram.Histogram;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,17 +12,20 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
 @RestController
 @RequestMapping("/bench")
 public class BenchmarkController {
-    private static Logger log = LoggerFactory.getLogger(BenchmarkController.class);
     @Autowired
     private UserService userService;
     @Autowired
     private UserService2 userService2;
 
-    @GetMapping("/service1/{count}")
-    public Mono<Void> callByBroker(@PathVariable("count") int count) {
+    @GetMapping("/serviceByBroker/{count}")
+    public Mono<String> callByBroker(@PathVariable("count") int count) {
         Histogram histogram = new Histogram(3600000000000L, 3);
         long start = System.nanoTime();
         return Flux.range(1, count)
@@ -35,17 +36,24 @@ public class BenchmarkController {
                     });
                 })
                 .subscribeOn(Schedulers.boundedElastic())
-                .doFinally(signalType -> {
-                    histogram.outputPercentileDistribution(System.out, 1000.0d);
+                .then(Mono.create(sink -> {
                     double completedMillis = (System.nanoTime() - start) / 1_000_000d;
+                    ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                    PrintStream output = new PrintStream(bos);
+                    histogram.outputPercentileDistribution(output, 1000.0d);
                     double tps = count / (completedMillis / 1_000d);
-                    log.info("Request by broker complete {} in {} ms", count, completedMillis);
-                    log.info("Request by broker tps {}", tps);
-                }).then();
+                    try {
+                        output.write(String.format("Request by broker complete %s in %s ms\n", count, completedMillis).getBytes(StandardCharsets.UTF_8));
+                        output.write(String.format("Request by broker tps %s\n", tps).getBytes(StandardCharsets.UTF_8));
+                    } catch (Exception ignore) {
+
+                    }
+                    sink.success(bos.toString());
+                }));
     }
 
-    @GetMapping("/service2/{count}")
-    public Mono<Void> callByDirect(@PathVariable("count") int count) {
+    @GetMapping("/serviceByDirect/{count}")
+    public Mono<String> callByDirect(@PathVariable("count") int count) {
         Histogram histogram = new Histogram(3600000000000L, 3);
         long start = System.nanoTime();
         return Flux.range(1, count)
@@ -56,12 +64,19 @@ public class BenchmarkController {
                     });
                 })
                 .subscribeOn(Schedulers.boundedElastic())
-                .doFinally(signalType -> {
-                    histogram.outputPercentileDistribution(System.out, 1000.0d);
+                .then(Mono.create(sink -> {
                     double completedMillis = (System.nanoTime() - start) / 1_000_000d;
+                    ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                    PrintStream output = new PrintStream(bos);
+                    histogram.outputPercentileDistribution(output, 1000.0d);
                     double tps = count / (completedMillis / 1_000d);
-                    log.info("Request by direct complete {} in {} ms", count, completedMillis);
-                    log.info("Request by direct tps {}", tps);
-                }).then();
+                    try {
+                        output.write(String.format("Request by direct complete %s in %s ms\n", count, completedMillis).getBytes(StandardCharsets.UTF_8));
+                        output.write(String.format("Request by direct tps %s\n", tps).getBytes(StandardCharsets.UTF_8));
+                    } catch (Exception ignore) {
+
+                    }
+                    sink.success(bos.toString());
+                }));
     }
 }
