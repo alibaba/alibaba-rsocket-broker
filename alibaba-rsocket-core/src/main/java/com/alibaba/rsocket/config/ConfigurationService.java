b35ccf88f31041d0a5631d0f545ec8ffe15075c7
leijuan <jacky.chenlb@alibaba-inc.com>
2021-05-01 17:50:35 -0700

    Add javadoc

--- alibaba-rsocket-core/src/main/java/com/alibaba/rsocket/config/ConfigurationService.java
+++ alibaba-rsocket-core/src/main/java/com/alibaba/rsocket/config/ConfigurationService.java
@@ -9,5 +9,12 @@ import reactor.core.publisher.Mono;
  * @author leijuan
  */
 public interface ConfigurationService {
+    /**
+     * get app's configuration
+     *
+     * @param appName app name
+     * @param key     key for configuration, such as application.properties
+     * @return configuration's content
+     */
     Mono<String> get(@NotNull String appName, @NotNull String key);
 }
