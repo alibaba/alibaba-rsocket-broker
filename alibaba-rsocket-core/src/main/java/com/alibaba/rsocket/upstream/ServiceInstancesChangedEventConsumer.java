51dd2d3d0da22c12b8c16e21ff851abe7b3e226b
leijuan <jacky.chenlb@alibaba-inc.com>
2021-05-06 21:29:24 -0700

    ServiceInstancesChangedEvent consumer

---
+++ alibaba-rsocket-core/src/main/java/com/alibaba/rsocket/upstream/ServiceInstancesChangedEventConsumer.java
@@ -0,0 +1,49 @@
+package com.alibaba.rsocket.upstream;
+
+import com.alibaba.rsocket.ServiceLocator;
+import com.alibaba.rsocket.cloudevents.CloudEventImpl;
+import com.alibaba.rsocket.events.CloudEventSupport;
+import com.alibaba.rsocket.events.CloudEventsConsumer;
+import com.alibaba.rsocket.observability.RsocketErrorCode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Mono;
+
+/**
+ * ServiceInstancesChangedEvent consumer
+ *
+ * @author leijuan
+ */
+public class ServiceInstancesChangedEventConsumer implements CloudEventsConsumer {
+    private static Logger log = LoggerFactory.getLogger(ServiceInstancesChangedEventConsumer.class);
+    private UpstreamManager upstreamManager;
+
+    public ServiceInstancesChangedEventConsumer(UpstreamManager upstreamManager) {
+        this.upstreamManager = upstreamManager;
+    }
+
+    @Override
+    public boolean shouldAccept(CloudEventImpl<?> cloudEvent) {
+        String type = cloudEvent.getAttributes().getType();
+        return ServiceInstancesChangedEvent.class.getCanonicalName().equalsIgnoreCase(type);
+    }
+
+    @Override
+    public Mono<Void> accept(CloudEventImpl<?> cloudEvent) {
+        return Mono.fromRunnable(() -> {
+            handleServicesChangedEvent(cloudEvent);
+        });
+    }
+
+    public void handleServicesChangedEvent(CloudEventImpl<?> cloudEvent) {
+        ServiceInstancesChangedEvent serviceInstancesChangedEvent = CloudEventSupport.unwrapData(cloudEvent, ServiceInstancesChangedEvent.class);
+        if (serviceInstancesChangedEvent != null) {
+            String serviceId = ServiceLocator.serviceId(serviceInstancesChangedEvent.getGroup(), serviceInstancesChangedEvent.getService(), serviceInstancesChangedEvent.getVersion());
+            UpstreamCluster upstreamCluster = upstreamManager.findClusterByServiceId(serviceId);
+            if (upstreamCluster != null) {
+                upstreamCluster.setUris(serviceInstancesChangedEvent.getUris());
+                log.info(RsocketErrorCode.message("RST-300202", serviceId, String.join(",", serviceInstancesChangedEvent.getUris())));
+            }
+        }
+    }
+}
