a93dab75598020a4c9859f0ab3549c99caae95cf
leijuan <jacky.chenlb@alibaba-inc.com>
2021-09-03 13:17:08 -0700

    Add json object decode support for single param

--- alibaba-rsocket-core/src/main/java/com/alibaba/rsocket/encoding/impl/ObjectEncodingHandlerJsonImpl.java
+++ alibaba-rsocket-core/src/main/java/com/alibaba/rsocket/encoding/impl/ObjectEncodingHandlerJsonImpl.java
@@ -60,7 +60,12 @@ public class ObjectEncodingHandlerJsonImpl implements ObjectEncodingHandler {
     public Object decodeParams(ByteBuf data, @Nullable Class<?>... targetClasses) throws EncodingException {
         if (data.readableBytes() > 0 && !isArrayEmpty(targetClasses)) {
             try {
-                return JsonUtils.readJsonArray(data, targetClasses);
+                final byte firstByte = data.getByte(0);
+                if (firstByte == '[') {
+                    return JsonUtils.readJsonArray(data, targetClasses);
+                } else {
+                    return JsonUtils.readJsonValue(data, targetClasses[0]);
+                }
             } catch (Exception e) {
                 throw new EncodingException(RsocketErrorCode.message("RST-700501", "bytebuf", Arrays.toString(targetClasses)), e);
             }
