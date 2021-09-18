2357c877120c4be611819bd736aae4e70902d65a
removedporn <86824510+removedporn@users.noreply.github.com>
2021-09-18 17:15:18 +0800

    Delete example directory

--- example/rsocket-requester/src/main/resources/humans.md
+++
@@ -1,4 +0,0 @@
-# RSocket Requester tester
-
-RSocket请求方，发起到Broker的连接，调用相关的RSocket服务
-
--- example/rsocket-responder/src/test/java/com/alibaba/spring/boot/rsocket/demo/SpringBootBaseTest.java
+++
@@ -1,9 +0,0 @@
-package com.alibaba.spring.boot.rsocket.demo;
-
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
-
-@SpringBootTest
-@TestPropertySource(properties = {"rsocket.disabled=true"})
-public abstract class SpringBootBaseTest {
-}
