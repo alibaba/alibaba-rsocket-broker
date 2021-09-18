8f3b56f3f483fdba849d338d7c530f973a50f0a5
removedporn <86824510+removedporn@users.noreply.github.com>
2021-09-18 11:33:19 +0800

    Delete alibaba-broker-server directory

--- alibaba-broker-server/src/main/build/bin/shutdown.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-PRGDIR=$(dirname "$0")
-kill $(cat "${PRGDIR}"/app.pid)
-rm -rf "${PRGDIR}"/app.pid
-echo "Alibaba RSocket Server Stopped."
--- alibaba-broker-server/src/main/build/bin/startup.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-@REM The script path to reference the included JRE java file
-SET SCRIPT_PATH=%~dp0
-
-SET JAR_FILE="%SCRIPT_PATH%\lib\*"
-
-java -classpath %JAR_FILE% com.alibaba.rsocket.broker.AlibabaRSocketBrokerServer
