2c0cd17f604b62b8364e262e6ea44192d2127ac0
Libing Chen <libing.chen@gmail.com>
2022-11-29 14:06:55 +0800

    chore: code format

--- example/rsocket-requester/src/main/java/com/alibaba/spring/boot/rsocket/demo/OpsController.java
+++ example/rsocket-requester/src/main/java/com/alibaba/spring/boot/rsocket/demo/OpsController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Sinks;
-import reactor.extra.processor.TopicProcessor;
 
 import java.util.Arrays;
 
